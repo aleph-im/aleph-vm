77c8ef67062ea099a049b1dab87021c87c83eba2
Hugo Herter <git@hugoherter.com>
2021-05-07 17:45:41 +0200

    Guest API catch all paths

--- guest_api/__main__.py
+++ guest_api/__main__.py
@@ -4,11 +4,11 @@ from aiohttp import web
 logging.basicConfig(level=logging.DEBUG)
 
 
-async def hello(request):
+async def proxy(request):
     return web.json_response({"Hello": "From API"})
 
 
 def run_guest_api(unix_socket_path):
     app = web.Application()
-    app.router.add_get('/get', hello)
+    app.router.add_route(method='*', path='/{tail:.*}', handler=proxy)
     web.run_app(app=app, path=unix_socket_path)
