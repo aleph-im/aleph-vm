78f593496ca6a18408680a4e0771318d94552b58
Hugo Herter <git@hugoherter.com>
2021-05-21 15:05:35 +0200

    Enhancement: Test internet connection in example_fastapi_2

--- examples/example_fastapi_2/__init__.py
+++ examples/example_fastapi_2/__init__.py
@@ -1,8 +1,9 @@
 import logging
-from typing import Optional
 logger = logging.getLogger(__name__)
 
-logger.debug("")
+logger.debug("import aiohttp")
+import aiohttp
+
 
 logger.debug("import aleph_client")
 from aleph_client.asynchronous import get_messages
@@ -13,32 +14,27 @@ logger.debug("imports done")
 app = FastAPI()
 
 
-async def get_data_http():
-    return "Have a look at <b>/messages</b>"
-
-
 @app.get("/")
 async def index():
-    data = await get_data_http()
     return {
         "Example": "example_fastapi_2",
-        "endpoints": ["/messages", "/run/{item_id}"],
+        "endpoints": ["/messages", "/internet"],
     }
 
 
 @app.get("/messages")
 async def read_aleph_messages():
+    """Read data from Aleph using the Aleph Client library."""
     data = await get_messages(
         hashes=["f246f873c3e0f637a15c566e7a465d2ecbb83eaa024d54ccb8fb566b549a929e"]
     )
     return {"Messages": data}
 
 
-@app.get("/run/{item_id}")
-def read_item(item_id: str, q: Optional[str] = None):
-    return {"pyz item_id": item_id, "q": q}
-
-
-@app.post("/run/{item_id}")
-def read_item_post(item_id: str, q: Optional[str] = None):
-    return {"pyz item_id_post": item_id, "q": q}
+@app.get("/internet")
+async def read_internet():
+    """Read data from the public Internet using aiohttp."""
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector()) as session:
+        async with session.get("https://aleph.im/") as resp:
+            resp.raise_for_status()
+            return {"result": resp.status, "headers": resp.headers}
