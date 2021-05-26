7e1d9fd68cadffdd3208bc98c413509aa569e5fe
Hugo Herter <git@hugoherter.com>
2021-05-26 17:27:21 +0200

    Fix: Run benchmarks with a real message ref

--- vm_supervisor/__main__.py
+++ vm_supervisor/__main__.py
@@ -98,12 +98,12 @@ async def benchmark(runs: int):
     """Measure performance by immediately running the supervisor
     with fake requests.
     """
-    ref = "vmid"
+    ref = "7dbf00da1c2b88ab32c0ba98504b499d13dceb62abdb928022f8420fc5d692a4"
 
     class FakeRequest: pass
 
     fake_request = FakeRequest()
-    fake_request.match_info = {"ref": ref, "suffix": "/path"}
+    fake_request.match_info = {"ref": ref, "suffix": "/"}
     fake_request.method = "GET"
     fake_request.query_string = ""
     fake_request.headers = []
@@ -115,7 +115,9 @@ async def benchmark(runs: int):
 
     for run in range(runs):
         t0 = time.time()
-        response: Response = await supervisor.run_code(message_ref=ref, request=fake_request)
+        response: Response = await supervisor.run_code(message_ref=ref,
+                                                       path="/",
+                                                       request=fake_request)
         assert response.status == 200
         bench.append(time.time() - t0)
 
