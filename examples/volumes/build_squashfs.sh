06a82eb44d14db15ff5f16bdc756da07e9b3ef98
Hugo Herter <git@hugoherter.com>
2021-06-11 16:12:40 +0200

    Fix: Add support for Docker in SquashFS build

--- examples/volumes/build_squashfs.sh
+++ examples/volumes/build_squashfs.sh
@@ -2,5 +2,13 @@
 
 set -euf
 
-podman build -t aleph-vm-build-squashfs .
-podman run --rm -ti -v "$( dirname "$0" )":/mnt aleph-vm-build-squashfs
+# Use Podman if installed, else use Docker
+if hash podman 2> /dev/null
+then
+  DOCKER_COMMAND=podman
+else
+  DOCKER_COMMAND=docker
+fi
+
+$DOCKER_COMMAND build -t aleph-vm-build-squashfs .
+$DOCKER_COMMAND run --rm -v "$(pwd)":/mnt aleph-vm-build-squashfs
