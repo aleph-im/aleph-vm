500dfcc7d233def6ad602d4baf678e0d619d3853
Hugo Herter <git@hugoherter.com>
2021-06-14 13:10:35 +0200

    Fix: Improve script syntax and name container

--- docker/run_vm_connector.sh
+++ docker/run_vm_connector.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+set -euf
+
 docker build -t aleph-connector -f docker/vm_connector.dockerfile .
+
 docker run -ti --rm -p 8000:8000/tcp \
-  -v $(pwd)/kernels:/opt/kernels:ro \
-  -v $(pwd)/vm_connector:/opt/vm_connector:ro \
-  aleph-connector $@
+  -v "$(pwd)/kernels:/opt/kernels:ro" \
+  -v "$(pwd)/vm_connector:/opt/vm_connector:ro" \
+  --name aleph-connector \
+  aleph-connector "$@"
