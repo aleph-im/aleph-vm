1c3e7dc45da7a55d029ee1c3a0fa1405c8f1b7ae
Hugo Herter <git@hugoherter.com>
2021-07-07 09:44:00 +0200

    Fix: Use UUID as model ids in Django example

--- examples/example_django/blog/models.py
+++ examples/example_django/blog/models.py
@@ -1,8 +1,10 @@
+import uuid
+
 from django.db import models
 
 
 class Article(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=256, help_text="Title of the blog article")
     body = models.TextField(help_text="Body of the blog article")
     date = models.DateTimeField(auto_created=True)
@@ -12,10 +14,10 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.CharField(max_length=1024)
     article = models.ForeignKey(to=Article, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_created=True, auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self):
         return f"Comment on {self.article.title}"
