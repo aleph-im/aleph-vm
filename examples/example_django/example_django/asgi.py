e6a724b1d3f2a12c05f6819d83f6d36f03a985a2
Hugo Herter <git@hugoherter.com>
2021-07-07 09:44:45 +0200

    Feature: Initialize DB and load fixture in Django example
    Closes #54

--- examples/example_django/example_django/asgi.py
+++ examples/example_django/example_django/asgi.py
@@ -14,3 +14,8 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_django.settings")
 
 application = get_asgi_application()
+
+os.system("/usr/bin/python3 /opt/code/manage.py migrate")
+
+os.system("/usr/bin/python3 /opt/code/manage.py "
+          "loaddata /opt/code/blog/fixtures/default_articles.json")
