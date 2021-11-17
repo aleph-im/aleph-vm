07df9f0aae1e9658f2e786b6d8361dd39252822e
Hugo Herter <git@hugoherter.com>
2021-11-17 17:12:47 +0100

    Feature: Improve developer tools with Docker supervisor image

--- docker/run_vm_supervisor.sh
+++ docker/run_vm_supervisor.sh
@@ -8,12 +8,11 @@ else
   DOCKER_COMMAND=docker
 fi
 
-$DOCKER_COMMAND build -ti -t aleph-vm-supervisor -f docker/vm_supervisor.dockerfile .
+$DOCKER_COMMAND build -t alephim/vm-supervisor-dev -f docker/vm_supervisor-dev.dockerfile .
 
 $DOCKER_COMMAND run -ti --rm \
-  -v $(pwd):/root/aleph-vm \
+  -v "$(pwd)/runtimes/aleph-debian-11-python/rootfs.squashfs:/opt/aleph-vm/runtimes/aleph-debian-11-python/rootfs.squashfs:ro" \
+  -v "$(pwd)/examples/volumes/volume-venv.squashfs:/opt/aleph-vm/examples/volumes/volume-venv.squashfs:ro" \
   --device /dev/kvm \
   -p 4020:4020 \
-  aleph-vm-supervisor \
-  bash
-#  python3 -m vm_supervisor -p -vv --system-logs --profile -f ./examples/example_fastapi_2
+  alephim/vm-supervisor-dev
