5453675ac6991fe63aa6edd63f9f044dbfc35815
Hugo Herter <git@hugoherter.com>
2021-11-17 17:10:35 +0100

    Problem: Podman was not supported in Docker launch scripts

--- docker/run_vm_connector.sh
+++ docker/run_vm_connector.sh
@@ -2,9 +2,17 @@
 
 set -euf
 
-docker build -t aleph-connector -f docker/vm_connector.dockerfile .
+# Use Podman if installed, else use Docker
+if hash podman 2> /dev/null
+then
+  DOCKER_COMMAND=podman
+else
+  DOCKER_COMMAND=docker
+fi
 
-docker run -ti --rm -p 8000:8000/tcp \
+$DOCKER_COMMAND build -t aleph-connector -f docker/vm_connector.dockerfile .
+
+$DOCKER_COMMAND run -ti --rm -p 4021:4021/tcp \
   -v "$(pwd)/kernels:/opt/kernels:ro" \
   -v "$(pwd)/vm_connector:/opt/vm_connector:ro" \
   --name aleph-connector \
