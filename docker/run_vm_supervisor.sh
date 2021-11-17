9143d3dc29bbd9e5d4b5442cf83cf9757ec96a8e
Hugo Herter <git@hugoherter.com>
2021-11-17 17:09:12 +0100

    Change: Improve running supervisor in a container

--- docker/run_vm_supervisor.sh
+++ docker/run_vm_supervisor.sh
@@ -4,5 +4,7 @@ podman build -ti -t aleph-vm-supervisor -f docker/vm_supervisor.dockerfile .
 podman run -ti --rm \
   -v $(pwd):/root/aleph-vm \
   --device /dev/kvm \
+  -p 4020:4020 \
   aleph-vm-supervisor \
-  python3 -m vm_supervisor -p -vv --system-logs --benchmark 1 --profile
+  bash
+#  python3 -m vm_supervisor -p -vv --system-logs --profile -f ./examples/example_fastapi_2
