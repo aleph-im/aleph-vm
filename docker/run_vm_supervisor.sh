048ebe2306de52b044c161d26f2fcbc1a256df45
Hugo Herter <git@hugoherter.com>
2021-11-17 17:10:35 +0100

    Problem: Podman was not supported in Docker launch scripts

--- docker/run_vm_supervisor.sh
+++ docker/run_vm_supervisor.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-podman build -ti -t aleph-vm-supervisor -f docker/vm_supervisor.dockerfile .
-podman run -ti --rm \
+# Use Podman if installed, else use Docker
+if hash podman 2> /dev/null
+then
+  DOCKER_COMMAND=podman
+else
+  DOCKER_COMMAND=docker
+fi
+
+$DOCKER_COMMAND build -ti -t aleph-vm-supervisor -f docker/vm_supervisor.dockerfile .
+
+$DOCKER_COMMAND run -ti --rm \
   -v $(pwd):/root/aleph-vm \
   --device /dev/kvm \
   -p 4020:4020 \
