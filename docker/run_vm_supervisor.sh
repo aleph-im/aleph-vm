6d70761b1ef83ae09b702ac82e473af2ce00ec06
Hugo Herter <git@hugoherter.com>
2022-01-14 17:17:54 +0100

    Internal: Mount vm_supervisor in container for faster development

--- docker/run_vm_supervisor.sh
+++ docker/run_vm_supervisor.sh
@@ -13,6 +13,7 @@ $DOCKER_COMMAND build -t alephim/vm-supervisor-dev -f docker/vm_supervisor-dev.d
 $DOCKER_COMMAND run -ti --rm \
   -v "$(pwd)/runtimes/aleph-debian-11-python/rootfs.squashfs:/opt/aleph-vm/runtimes/aleph-debian-11-python/rootfs.squashfs:ro" \
   -v "$(pwd)/examples/volumes/volume-venv.squashfs:/opt/aleph-vm/examples/volumes/volume-venv.squashfs:ro" \
+  -v "$(pwd)/vm_supervisor:/opt/aleph-vm/vm_supervisor:ro" \
   --device /dev/kvm \
   -p 4020:4020 \
   alephim/vm-supervisor-dev
