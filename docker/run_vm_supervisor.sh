9f48eb31da0e81e66c28c85cebaab79d2b514e12
Hugo Herter <git@hugoherter.com>
2022-01-26 17:28:57 +0100

    Internal: Package `firecracker` was not mounted in development container

--- docker/run_vm_supervisor.sh
+++ docker/run_vm_supervisor.sh
@@ -14,6 +14,7 @@ $DOCKER_COMMAND run -ti --rm \
   -v "$(pwd)/runtimes/aleph-debian-11-python/rootfs.squashfs:/opt/aleph-vm/runtimes/aleph-debian-11-python/rootfs.squashfs:ro" \
   -v "$(pwd)/examples/volumes/volume-venv.squashfs:/opt/aleph-vm/examples/volumes/volume-venv.squashfs:ro" \
   -v "$(pwd)/vm_supervisor:/opt/aleph-vm/vm_supervisor:ro" \
+  -v "$(pwd)/firecracker:/opt/aleph-vm/firecracker:ro" \
   --device /dev/kvm \
   -p 4020:4020 \
   alephim/vm-supervisor-dev
