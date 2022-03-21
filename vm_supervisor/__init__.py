45405b22d58ef54c909a277febcfc95fded2dafb
Hugo Herter <git@hugoherter.com>
2022-03-21 18:05:32 +0100

    Problem: Users could not know the version of Aleph-VM for diagnostic
    Solution: Add the version of the software in the HTTP response headers

--- vm_supervisor/__init__.py
+++ vm_supervisor/__init__.py
@@ -1,1 +1,12 @@
+from subprocess import check_output
+
+
+def get_version_from_git() -> str:
+    return check_output(("git", "describe", "--tags")).strip().decode()
+
+
+# The version number is harcoded in the following line when packaging the software
+__version__ = get_version_from_git()
+
+
 from . import supervisor
