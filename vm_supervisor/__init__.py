4aa423786aacc7aeb7f1830fc099fa981719c427
Hugo Herter <git@hugoherter.com>
2022-04-26 19:30:55 +0200

    Fix: Getting version from git crashed when git was missing or on shallow clone

--- vm_supervisor/__init__.py
+++ vm_supervisor/__init__.py
@@ -1,8 +1,18 @@
-from subprocess import check_output
+import logging
+from subprocess import check_output, CalledProcessError
+
+logger = logging.getLogger(__name__)
 
 
 def get_version_from_git() -> str:
-    return check_output(("git", "describe", "--tags")).strip().decode()
+    try:
+        return check_output(("git", "describe", "--tags")).strip().decode()
+    except FileNotFoundError:
+        logger.warning("git not found")
+        return "unknown-version"
+    except CalledProcessError:
+        logger.warning("git description not available")
+        return "unavailable-version"
 
 
 # The version number is harcoded in the following line when packaging the software
