658bc51ac0ef8a67e2cab363cbee6fa049eee25b
Hugo Herter <git@hugoherter.com>
2022-05-11 17:06:32 +0200

    Feature: Allow to run CI in a DigitalOcean Droplet
    These support nested virtualization, which GitHub Actions does not.

---
+++ .github/scripts/extract_droplet_ipv4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Extract the IP address of a DigitalOcean Droplet
+from the JSON returned by `doctl compute droplet get $name --output json
+"""
+
+import json
+import sys
+
+droplet_info = json.load(sys.stdin)
+print(droplet_info[0]["networks"]["v4"][0]["ip_address"])
