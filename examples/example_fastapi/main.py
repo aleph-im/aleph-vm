76afc09a9fc56bcef01c52afde846ae64200b731
Hugo Herter <git@hugoherter.com>
2022-05-11 16:02:00 +0200

    Change: Use the load balancer instead of api2
    This should make requests more reliable with high availability.

--- examples/example_fastapi/main.py
+++ examples/example_fastapi/main.py
@@ -151,7 +151,7 @@ filters = [{
 async def aleph_event(event):
     print("aleph_event", event)
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector()) as session:
-        async with session.get("https://api2.aleph.im/api/v0/info/public.json") as resp:
+        async with session.get("https://official.aleph.cloud/api/v0/info/public.json") as resp:
             print('RESP', resp)
             resp.raise_for_status()
     return {
