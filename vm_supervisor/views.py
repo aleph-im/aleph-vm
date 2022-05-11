e29ece4ac2e8d57a071a189ad9dcede75de5aa69
Hugo Herter <git@hugoherter.com>
2022-05-11 15:36:08 +0200

    Fix: Response body requires bytes, was given str

--- vm_supervisor/views.py
+++ vm_supervisor/views.py
@@ -77,7 +77,7 @@ async def run_code_from_hostname(request: web.Request) -> web.Response:
 def authenticate_request(request: web.Request) -> web.Response:
     """Check that the token in the cookies matches the app's secret token."""
     if request.cookies.get("token") != request.app["secret_token"]:
-        raise web.HTTPUnauthorized(reason="Invalid token", body="401 Invalid token")
+        raise web.HTTPUnauthorized(reason="Invalid token", text="401 Invalid token")
 
 
 async def about_login(request: web.Request) -> web.Response:
