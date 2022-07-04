0dca97ce6ec3567b80b542a323eabcb45122a050
Hugo Herter <git@hugoherter.com>
2022-07-04 19:26:29 +0200

    Fix: VM id counter values too high could cause issues in IP ranges

--- vm_supervisor/pool.py
+++ vm_supervisor/pool.py
@@ -1,8 +1,9 @@
 import asyncio
 import logging
-from typing import Dict, Optional, List
+from typing import Dict, Optional, Iterable
 
 from aleph_message.models import ProgramContent, ProgramMessage
+
 from .conf import settings
 from .models import VmHash, VmExecution
 
@@ -33,9 +34,33 @@ class VmPool:
         execution = VmExecution(vm_hash=vm_hash, program=program, original=original)
         self.executions[vm_hash] = execution
         await execution.prepare()
-        self.counter += 1
-        await execution.create(address=self.counter)
+        vm_id = self.get_unique_vm_id()
+        await execution.create(vm_id=vm_id)
         return execution
+
+    def get_unique_vm_id(self) -> int:
+        """Get a unique identifier for the VM.
+
+        This identifier is used to name the network interface and in the IPv4 range
+        dedicated to the VM.
+        """
+        self.counter += 1
+        if self.counter < 255**2:
+            # In common cases, use the counter itself as the vm_id. This makes it
+            # easier to debug.
+            return self.counter
+        else:
+            # The value of the counter is too high and some functions such as the
+            # IPv4 range dedicated to the VM do not support such high values.
+            #
+            # We therefore recycle vm_id values from executions that are not running
+            # anymore.
+            currently_used_vm_ids = set(execution.vm.vm_id
+                                        for execution in self.executions.values()
+                                        if execution.is_running)
+            for i in range(settings.START_ID_INDEX, 255**2):
+                if i not in currently_used_vm_ids:
+                    return i
 
     async def get_running_vm(self, vm_hash: VmHash) -> Optional[VmExecution]:
         """Return a running VM or None. Disables the VM expiration task."""
@@ -47,6 +72,12 @@ class VmPool:
             return None
 
     def forget_vm(self, vm_hash: VmHash) -> None:
+        """Remove a VM from the executions pool.
+
+        Used after self.create_a_vm(...) raised an error in order to
+        completely forget about the execution and enforce a new execution
+        when attempted again.
+        """
         try:
             del self.executions[vm_hash]
         except KeyError:
