da986425596f1294e54eb9ab5d73ae0ceeb09518
Aneeque <aneequezubair9@gmail.com>
2022-12-28 12:19:48 +0100

    reviewing the fishnet

--- examples/example_django/example_django/settings.py
+++ examples/example_django/example_django/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "bootstrap5",
     "django.contrib.staticfiles",
     "blog",
 ]
