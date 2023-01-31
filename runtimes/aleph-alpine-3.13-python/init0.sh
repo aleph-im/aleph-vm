c096f198935b6d3fd9a93f4cfb2b7508b9a58f20
Hugo Herter <git@hugoherter.com>
2023-01-31 01:03:16 +0100

    Fix: Docker containers could not run in runtime
    Solution: Mount cgroups, configure iptables and install dependencies

--- runtimes/aleph-alpine-3.13-python/init0.sh
+++ runtimes/aleph-alpine-3.13-python/init0.sh
@@ -29,6 +29,15 @@ mount -t tmpfs run /run -o mode=0755,nosuid,nodev
 mount -t devpts devpts /dev/pts -o mode=0620,gid=5,nosuid,noexec
 mount -t tmpfs shm /dev/shm -omode=1777,nosuid,nodev
 
+# Required by Docker
+cgroupfs-mount
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+
+# Enable the following to force the storage driver used by Docker.
+# See https://docs.docker.com/storage/storagedriver/select-storage-driver/
+#echo '{\n"storage-driver": "overlay2"\n}\n' > /etc/docker/daemon.json
+
 # List block devices
 lsblk
 
