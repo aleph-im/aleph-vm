eb112d753ce497ad5572e93325431f6436e11f0e
Hugo Herter <git@hugoherter.com>
2023-03-07 21:04:48 +0100

    Work in progress

--- packaging/nixos/setup.sh
+++ packaging/nixos/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -euf -o pipefail
 
 mkdir -p /opt/firecracker
 mkdir -p /opt/firecracker-release
@@ -7,3 +9,5 @@ curl -o /opt/firecracker/vmlinux.bin "https://s3.amazonaws.com/spec.ccfc.min/img
 curl -fsSL https://github.com/firecracker-microvm/firecracker/releases/download/v1.1.0/firecracker-v1.1.0-x86_64.tgz | tar -xz --directory /opt/firecracker-release
 cp /opt/firecracker-release/release-v*/firecracker-v* /opt/firecracker/firecracker
 cp /opt/firecracker-release/release-v*/jailer-v* /opt/firecracker/jailer
+
+cp $(find /nix/store/ | grep nftables-1.0.5 | grep libnftables.so.1.1 | head -n 1) ./libnftables.so.1
