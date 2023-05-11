6142494f658a3a6a19575809ec9db5f678848f9d
Olivier Desenfans <desenfans.olivier@gmail.com>
2023-05-11 17:20:40 +0200

    Fix: support running migrations on existing CRNs
    Problem: the migration script introduced with alembic assumes
    that the records table does not exist yet. This makes it impossible
    to run the new version on existing CRNs as the table exists,
    making the migration statement fail.
    
    The network_tap column was also deleted in a previous commit
    after 0.2.5, but the libsqlite3 version shipped with Ubuntu 20.04
    does not support `ALTER TABLE ... DROP COLUMN` statements.
    
    Solution: check if the table exists in the migration script.

--- vm_supervisor/models.py
+++ vm_supervisor/models.py
@@ -233,6 +233,7 @@ class VmExecution:
                     io_write_bytes=pid_info["process"]["io_counters"][3],
                     vcpus=self.vm.hardware_resources.vcpus,
                     memory=self.vm.hardware_resources.memory,
+                    network_tap=self.vm.tap_interface.device_name,
                 )
             )
         else:
