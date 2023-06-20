40a8940d18cabd8a0ff53645cb1665eefac1071a
Olivier Desenfans <desenfans.olivier@gmail.com>
2023-06-20 10:21:17 +0200

    Feature: VMs did not support IPv6
    Problem: We want each VM to be reachable on the internet, with
    no restriction in terms of ports, protocols, etc.
    
    Solution: Slice up the IPv6 range attributed to each CRN and allocate
    a subnet to each VM.
    
    Add the support for NDP proxy for some hosting providers.
    
    IPv6 defaults to local addresses for broad compatibility, opt-in public addresses.
    
    The custom init `init1.py` is updated to support IPv6.
    
    Separate loopback networking and always run it
    - Remove duplicate IPv4 setting
    - Only setup IPv4 route if IPv4 is set
    - Only setup eth0 if ip or ipv6
    
    Co-authored-by: Hugo Herter <git@hugoherter.com>

--- vm_supervisor/network/interfaces.py
+++ vm_supervisor/network/interfaces.py
@@ -1,10 +1,11 @@
 import asyncio
 import logging
 import shutil
-from ipaddress import IPv4Interface
+from ipaddress import IPv4Interface, IPv6Address, IPv6Interface, IPv6Network
 from subprocess import run
 
 from .ipaddresses import IPv4NetworkWithInterfaces
+from .ndp_proxy import NdpProxy
 
 logger = logging.getLogger(__name__)
 
@@ -12,10 +13,19 @@ logger = logging.getLogger(__name__)
 class TapInterface:
     device_name: str
     ip_network: IPv4NetworkWithInterfaces
+    ipv6_network: IPv6Network
 
-    def __init__(self, device_name: str, ip_network: IPv4NetworkWithInterfaces):
+    def __init__(
+        self,
+        device_name: str,
+        ip_network: IPv4NetworkWithInterfaces,
+        ipv6_network: IPv6Network,
+        ndp_proxy: NdpProxy,
+    ):
         self.device_name: str = device_name
         self.ip_network: IPv4NetworkWithInterfaces = ip_network
+        self.ipv6_network = ipv6_network
+        self.ndp_proxy = ndp_proxy
 
     @property
     def guest_ip(self) -> IPv4Interface:
@@ -24,6 +34,14 @@ class TapInterface:
     @property
     def host_ip(self) -> IPv4Interface:
         return self.ip_network[1]
+
+    @property
+    def guest_ipv6(self) -> IPv6Interface:
+        return IPv6Interface(f"{self.ipv6_network[1]}/{self.ipv6_network.prefixlen}")
+
+    @property
+    def host_ipv6(self) -> IPv6Interface:
+        return IPv6Interface(f"{self.ipv6_network[0]}/{self.ipv6_network.prefixlen}")
 
     async def create(self):
         logger.debug("Create network interface")
@@ -40,7 +58,19 @@ class TapInterface:
                 self.device_name,
             ]
         )
+        ipv6_gateway = self.host_ipv6
+        run(
+            [
+                ip_command,
+                "addr",
+                "add",
+                str(ipv6_gateway),
+                "dev",
+                self.device_name,
+            ]
+        )
         run([ip_command, "link", "set", self.device_name, "up"])
+        self.ndp_proxy.add_range(self.device_name, ipv6_gateway.network)
         logger.debug(f"Network interface created: {self.device_name}")
 
     async def delete(self) -> None:
@@ -48,4 +78,5 @@ class TapInterface:
         Then removes the interface from the host."""
         logger.debug(f"Removing interface {self.device_name}")
         await asyncio.sleep(0.1)  # Avoids Device/Resource busy bug
+        self.ndp_proxy.delete_range(self.device_name)
         run(["ip", "tuntap", "del", self.device_name, "mode", "tap"])
--- vm_supervisor/vm/firecracker/executable.py
+++ vm_supervisor/vm/firecracker/executable.py
@@ -182,11 +182,25 @@ class AlephFirecrackerExecutable(Generic[ConfigurationType]):
         self.guest_api_process = None
         self._firecracker_config = None
 
-    def get_vm_ip(self):
-        return self.tap_interface.guest_ip.with_prefixlen
+    def get_vm_ip(self) -> Optional[str]:
+        if self.tap_interface:
+            return self.tap_interface.guest_ip.with_prefixlen
+        return None
 
-    def get_vm_route(self):
-        return str(self.tap_interface.host_ip).split("/", 1)[0]
+    def get_vm_route(self) -> Optional[str]:
+        if self.tap_interface:
+            return str(self.tap_interface.host_ip).split("/", 1)[0]
+        return None
+
+    def get_vm_ipv6(self) -> Optional[str]:
+        if self.tap_interface:
+            return self.tap_interface.guest_ipv6.with_prefixlen
+        return None
+
+    def get_vm_ipv6_gateway(self) -> Optional[str]:
+        if self.tap_interface:
+            return str(self.tap_interface.host_ipv6.ip)
+        return None
 
     def to_dict(self):
         """Dict representation of the virtual machine. Used to record resource usage and for JSON serialization."""
