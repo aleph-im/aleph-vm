7d8eccf2989a70ee387f17e27fac95914fb6fae4
Hugo Herter <git@hugoherter.com>
2023-07-03 13:19:30 +0200

    Fix: NDP Proxy was sync and mandatory
    Problems:
    1. Operations on the NDP Proxy were synchronous.
    2. Developing on the supervisor required the installation of the NDP Proxy.
    
    Solutions:
    1. Make operations on the NDP Proxy asynchronous.
    2. Add setting `USE_NDP_PROXY` that permits users to disable the NDP Proxy.

--- vm_supervisor/network/ndp_proxy.py
+++ vm_supervisor/network/ndp_proxy.py
@@ -13,8 +13,9 @@ import logging
 from dataclasses import dataclass
 from ipaddress import IPv6Network
 from pathlib import Path
-from subprocess import run
 from typing import Dict
+
+from vm_supervisor.utils import run_in_subprocess
 
 logger = logging.getLogger(__name__)
 
@@ -30,28 +31,28 @@ class NdpProxy:
         self.interface_address_range_mapping: Dict[str, IPv6Network] = {}
 
     @staticmethod
-    def _restart_ndppd():
+    async def _restart_ndppd():
         logger.debug("Restarting ndppd")
-        run(["systemctl", "restart", "ndppd"])
+        await run_in_subprocess(["systemctl", "restart", "ndppd"])
 
-    def _update_ndppd_conf(self):
+    async def _update_ndppd_conf(self):
         config = f"proxy {self.host_network_interface} {{\n"
         for interface, address_range in self.interface_address_range_mapping.items():
             config += f"  rule {address_range} {{\n    iface {interface}\n  }}\n"
         config += "}\n"
         Path("/etc/ndppd.conf").write_text(config)
-        self._restart_ndppd()
+        await self._restart_ndppd()
 
-    def add_range(self, interface: str, address_range: IPv6Network):
+    async def add_range(self, interface: str, address_range: IPv6Network):
         logger.debug("Proxying range %s -> %s", address_range, interface)
         self.interface_address_range_mapping[interface] = address_range
-        self._update_ndppd_conf()
+        await self._update_ndppd_conf()
 
-    def delete_range(self, interface: str):
+    async def delete_range(self, interface: str):
         try:
             address_range = self.interface_address_range_mapping.pop(interface)
             logger.debug("Deactivated proxying for %s (%s)", interface, address_range)
         except KeyError:
             return
 
-        self._update_ndppd_conf()
+        await self._update_ndppd_conf()
