b4e736285f3c8dc90e16130750799e3396b4c1e3
Andres D. Molins <nesitor@gmail.com>
2023-07-15 11:29:01 +0200

    Feature: Forget old snapshots also before delete the file.

--- vm_supervisor/snapshots.py
+++ vm_supervisor/snapshots.py
@@ -2,12 +2,11 @@ import logging
 from pathlib import Path
 from typing import Optional
 
-from aleph_message.models import ItemHash, StoreMessage
-from aleph_message.status import MessageStatus
-
 from aleph.sdk.chains.common import get_fallback_account
 from aleph.sdk.client import AuthenticatedAlephClient
 from aleph.sdk.types import StorageEnum
+from aleph_message.models import ItemHash, StoreMessage
+from aleph_message.status import MessageStatus
 
 from .conf import SnapshotCompressionAlgorithm
 from .storage import compress_volume_snapshot, create_volume_snapshot
@@ -26,20 +25,46 @@ class DiskVolumeFile:
 
 class CompressedDiskVolumeSnapshot(DiskVolumeFile):
     algorithm: SnapshotCompressionAlgorithm
+    uploaded_item_hash: Optional[ItemHash]
 
-    def __init__(self, path: Path, algorithm: SnapshotCompressionAlgorithm):
+    def __init__(
+        self,
+        path: Path,
+        algorithm: SnapshotCompressionAlgorithm,
+        uploaded_item_hash: Optional[ItemHash] = None,
+    ):
         super().__init__(path=path)
         self.algorithm = algorithm
+        self.uploaded_item_hash = uploaded_item_hash
 
     def delete(self) -> None:
         self.path.unlink(missing_ok=True)
 
     async def upload(self, vm_hash: ItemHash) -> ItemHash:
         account = get_fallback_account()
-        async with AuthenticatedAlephClient(account=account, api_server="https://official.aleph.cloud") as client:
+        async with AuthenticatedAlephClient(
+            account=account, api_server="https://official.aleph.cloud"
+        ) as client:
             message, status = await client.create_store(
-                file_path=self.path, storage_engine=StorageEnum.ipfs, sync=True, ref=f"snapshot_{vm_hash}"
+                file_path=self.path,
+                storage_engine=StorageEnum.ipfs,
+                sync=True,
+                ref=f"snapshot_{vm_hash}",
             )
+            assert status == MessageStatus.PROCESSED
+            self.uploaded_item_hash = message.item_hash
+            return self.uploaded_item_hash
+
+    async def forget(self) -> None:
+        assert (
+            self.uploaded_item_hash
+        ), "CompressedDiskVolumeSnapshot item_hash not available"
+
+        account = get_fallback_account()
+        async with AuthenticatedAlephClient(
+            account=account, api_server="https://official.aleph.cloud"
+        ) as client:
+            message, status = await client.forget(hashes=[self.uploaded_item_hash])
             assert status == MessageStatus.PROCESSED
 
 
