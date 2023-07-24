dc48482e759757b54a308fc3f4ee03b195d31e02
Andres D. Molins <nesitor@gmail.com>
2023-07-24 11:16:15 +0200

    Feature: Implement IPFS upload for snapshots.

--- vm_supervisor/snapshots.py
+++ vm_supervisor/snapshots.py
@@ -1,10 +1,12 @@
 import logging
+
 from pathlib import Path
 from typing import Optional
 
 from aleph.sdk.chains.common import get_fallback_account
 from aleph.sdk.client import AuthenticatedAlephClient
 from aleph.sdk.types import StorageEnum
+
 from aleph_message.models import ItemHash, StoreMessage
 from aleph_message.status import MessageStatus
 
@@ -41,19 +43,10 @@ class CompressedDiskVolumeSnapshot(DiskVolumeFile):
         self.path.unlink(missing_ok=True)
 
     async def upload(self, vm_hash: ItemHash) -> ItemHash:
-        account = get_fallback_account()
-        async with AuthenticatedAlephClient(
-            account=account, api_server="https://official.aleph.cloud"
-        ) as client:
-            message, status = await client.create_store(
-                file_path=self.path,
-                storage_engine=StorageEnum.ipfs,
-                sync=True,
-                ref=f"snapshot_{vm_hash}",
-            )
-            assert status == MessageStatus.PROCESSED
-            self.uploaded_item_hash = message.item_hash
-            return self.uploaded_item_hash
+        ref = f"snapshot_{vm_hash}"
+        ipfs_hash = await ipfs_upload_file(self.path)
+        snapshot_item_hash = await send_store_ipfs_message(ipfs_hash, ref)
+        return snapshot_item_hash
 
     async def forget(self) -> None:
         assert (
@@ -92,3 +85,31 @@ class DiskVolume(DiskVolumeFile):
     async def take_snapshot(self) -> DiskVolumeSnapshot:
         snapshot = await create_volume_snapshot(self.path)
         return DiskVolumeSnapshot(snapshot)
+
+
+async def ipfs_upload_file(path: Path) -> str:
+    """Push a file to the IPFS service."""
+    account = get_fallback_account()
+    # Upload file to local IPFS node
+    async with AuthenticatedAlephClient(
+            account=account, api_server="http://127.0.0.1:5001"
+    ) as client:
+        ipfs_hash = await client.ipfs_push_file(
+            file_content=str(path),
+        )
+        return ipfs_hash
+
+
+async def send_store_ipfs_message(file_hash: str, ref: str) -> ItemHash:
+    account = get_fallback_account()
+    async with AuthenticatedAlephClient(
+            account=account, api_server="https://official.aleph.cloud"
+    ) as client:
+        message, status = await client.create_store(
+            file_hash=file_hash,
+            storage_engine=StorageEnum.ipfs,
+            sync=True,
+            ref=ref,
+        )
+        assert status == MessageStatus.PROCESSED
+        return message.item_hash
