1ff127f1e96dc2c2ed72eadef3c7441a3252b74d
Andres D. Molins <amolinsdiaz@yahoo.es>
2023-07-28 15:52:58 +0200

    Fix: Fixed cloud-init refresh problems and improved way to create volume files.

--- vm_supervisor/vm/firecracker/instance.py
+++ vm_supervisor/vm/firecracker/instance.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import logging
 from pathlib import Path
 from tempfile import NamedTemporaryFile
@@ -189,6 +190,8 @@ class AlephFirecrackerInstance(AlephFirecrackerExecutable):
             "disable_root": False,
             "ssh_pwauth": False,
             "ssh_authorized_keys": ssh_authorized_keys,
+            # Avoid the resize error because we already do it on the VM disk creation stage
+            "resize_rootfs": False,
         }
 
         cloud_config_header = "#cloud-config\n"
@@ -230,27 +233,42 @@ class AlephFirecrackerInstance(AlephFirecrackerExecutable):
             network, default_flow_style=False, sort_keys=False
         ).encode()
 
+    def _create_metadata_file(self) -> bytes:
+        """Creates metadata configuration file for cloud-init tool"""
+
+        metadata = {
+            "instance-id": f"iid-instance-{self.vm_id}",
+            "local-hostname": str(self.vm_hash),
+        }
+
+        return json.dumps(metadata).encode()
+
     async def _create_cloud_init_drive(self) -> Drive:
         """Creates the cloud-init volume to configure and setup the VM"""
 
         disk_image_path = settings.EXECUTION_ROOT / f"cloud-init-{self.vm_hash}.img"
 
-        with NamedTemporaryFile() as main_config_file:
+        with NamedTemporaryFile() as user_data_config_file:
             user_data = self._encode_user_data()
-            main_config_file.write(user_data)
-            main_config_file.flush()
+            user_data_config_file.write(user_data)
+            user_data_config_file.flush()
             with NamedTemporaryFile() as network_config_file:
                 network_config = self._create_network_file()
                 network_config_file.write(network_config)
                 network_config_file.flush()
+                with NamedTemporaryFile() as metadata_config_file:
+                    metadata_config = self._create_metadata_file()
+                    metadata_config_file.write(metadata_config)
+                    metadata_config_file.flush()
 
-                await run_in_subprocess(
-                    [
-                        "cloud-localds",
-                        f"--network-config={network_config_file.name}",
-                        str(disk_image_path),
-                        main_config_file.name,
-                    ]
-                )
+                    await run_in_subprocess(
+                        [
+                            "cloud-localds",
+                            f"--network-config={network_config_file.name}",
+                            str(disk_image_path),
+                            user_data_config_file.name,
+                            metadata_config_file.name,
+                        ]
+                    )
 
         return self.fvm.enable_drive(disk_image_path, read_only=True)
