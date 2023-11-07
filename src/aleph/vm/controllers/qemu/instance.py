f87b108664a4485b24b9f2476caf61cee64b531a
Olivier Le Thanh Duong <olivier@lethanh.be>
2023-11-07 09:48:21 +0100

    qemu cleanup, to_dict

--- src/aleph/vm/controllers/qemu/instance.py
+++ src/aleph/vm/controllers/qemu/instance.py
@@ -1,31 +1,29 @@
 import asyncio
 import logging
-import os
 import shutil
-import sys
+from asyncio.subprocess import Process
 from pathlib import Path
 from typing import Generic, Optional
 
 import psutil
-
-from aleph.vm.controllers.qemu import logger
-from aleph.vm.utils import run_in_subprocess
-
-from aleph.vm.orchestrator.vm.vm_type import VmType
-
-from aleph.vm.network.hostnetwork import make_ipv6_allocator
-
-from aleph.vm.network.ipaddresses import IPv4NetworkWithInterfaces
-
-from aleph.vm.conf import settings
-from aleph.vm.controllers.firecracker.executable import ConfigurationType, AlephFirecrackerResources
-from aleph.vm.controllers.qemu.cloudinit import CloudInitMixin
-from aleph.vm.network.firewall import teardown_nftables_for_vm
-from aleph.vm.network.interfaces import TapInterface
 from aleph_message.models import ItemHash
 from aleph_message.models.execution.environment import MachineResources
 from aleph_message.models.execution.instance import RootfsVolume
 from aleph_message.models.execution.volume import PersistentVolume
+
+from aleph.vm.conf import settings
+from aleph.vm.controllers.firecracker.executable import (
+    AlephFirecrackerResources,
+    ConfigurationType,
+)
+from aleph.vm.controllers.firecracker.snapshots import CompressedDiskVolumeSnapshot
+from aleph.vm.controllers.qemu.cloudinit import CloudInitMixin
+from aleph.vm.network.firewall import teardown_nftables_for_vm
+from aleph.vm.network.hostnetwork import make_ipv6_allocator
+from aleph.vm.network.interfaces import TapInterface
+from aleph.vm.network.ipaddresses import IPv4NetworkWithInterfaces
+from aleph.vm.orchestrator.vm.vm_type import VmType
+from aleph.vm.utils import run_in_subprocess
 
 logger = logging.getLogger(__name__)
 
@@ -69,18 +67,16 @@ class AlephQemuResources(AlephFirecrackerResources):
 class AlephQemuInstance(Generic[ConfigurationType], CloudInitMixin):
     vm_id: int
     vm_hash: ItemHash
-    # resources: AlephFirecrackerResources
+    resources: AlephQemuResources
     enable_console: bool
     enable_networking: bool
     hardware_resources: MachineResources
     tap_interface: Optional[TapInterface] = None
-    # fvm: MicroVM
-    # vm_configuration: Optional[ConfigurationType]
+    vm_configuration: Optional[ConfigurationType]
     # guest_api_process: Optional[Process] = None
     is_instance: bool
+    qemu_process: Process
     support_snapshot = False
-
-    # _firecracker_config: Optional[FirecrackerConfig] = None
 
     def __str__(self):
         return f"<AlephQemuInstance {self.vm_id}>"
@@ -105,18 +101,12 @@ class AlephQemuInstance(Generic[ConfigurationType], CloudInitMixin):
         self.hardware_resources = hardware_resources
         self.tap_interface = tap_interface
 
-        # These properties are set later in the setup and configuration.
-        self.vm_configuration = None
-        self.guest_api_process = None
-        # self._firecracker_config = None
-
     def to_dict(self):
         """Dict representation of the virtual machine. Used to record resource usage and for JSON serialization."""
-        assert 1 / 0
-        if self.fvm.proc and psutil:
+        if self.qemu_process and psutil:
             # The firecracker process is still running and process information can be obtained from `psutil`.
             try:
-                p = psutil.Process(self.fvm.proc.pid)
+                p = psutil.Process(self.qemu_process.pid)
                 pid_info = {
                     "status": p.status(),
                     "create_time": p.create_time(),
@@ -130,7 +120,7 @@ class AlephQemuInstance(Generic[ConfigurationType], CloudInitMixin):
                     "num_ctx_switches": p.num_ctx_switches(),
                 }
             except psutil.NoSuchProcess:
-                logger.warning("Cannot read process metrics (process not found)")
+                logger.warning("Cannot read process metrics (process %s not found)", self.qemu_process)
                 pid_info = None
         else:
             pid_info = None
@@ -141,8 +131,6 @@ class AlephQemuInstance(Generic[ConfigurationType], CloudInitMixin):
         }
 
     async def setup(self):
-        # self._firecracker_config = FirecrackerConfig(...)
-        # raise NotImplementedError()
         pass
 
     async def start(self):
@@ -156,8 +144,9 @@ class AlephQemuInstance(Generic[ConfigurationType], CloudInitMixin):
         vcpu_count = self.hardware_resources.vcpus
         mem_size_mib = self.hardware_resources.memory
         mem_size_mb = int(mem_size_mib / 1024 / 1024 * 1000 * 1000)
+        # hardware_resources.published ports -> not implemented at the moment
+        # hardware_resources.seconds -> only for microvm
 
-        # make the snapshot image
         args = [
             qemu_path,
             "-enable-kvm",
@@ -165,6 +154,7 @@ class AlephQemuInstance(Generic[ConfigurationType], CloudInitMixin):
             str(mem_size_mb),
             "-smp",
             str(vcpu_count),
+            # Disable floppy
             "-fda",
             "",
             # "-snapshot",  # Do not save anything to disk
@@ -173,6 +163,7 @@ class AlephQemuInstance(Generic[ConfigurationType], CloudInitMixin):
             "-display",
             "none",  # Comment for debug
         ]
+        # FIXME local HACK
         if not self.enable_networking:
             self.enable_networking = True
             self.tap_interface = TapInterface(
@@ -187,13 +178,8 @@ class AlephQemuInstance(Generic[ConfigurationType], CloudInitMixin):
             )
         if self.tap_interface:
             interface_name = self.tap_interface.device_name
-            # script=no, downscript=no are there so qemu don't try to set up the network itself
+            # script=no, downscript=no tell qemu not to try to set up the network itself
             args += ["-net", "nic,model=virtio", "-net", f"tap,ifname={interface_name},script=no,downscript=no"]
-
-        # TODO implement published ports?
-        # seconds -> only for microvm
-
-        # FIXME local HACK
 
         cloud_init_drive = await self._create_cloud_init_drive()
         if cloud_init_drive:
@@ -201,14 +187,14 @@ class AlephQemuInstance(Generic[ConfigurationType], CloudInitMixin):
 
         try:
             print(*args)
-            proc = await asyncio.create_subprocess_exec(
+            self.qemu_process = proc = await asyncio.create_subprocess_exec(
                 *args,
                 stdin=asyncio.subprocess.PIPE,
                 stdout=asyncio.subprocess.PIPE,
             )
 
             logger.debug(f"setup done {self}, {proc}")
-            stdout, stderr = await proc.communicate()
+            stdout, stderr = await proc.communicate() # FIXME: should not be there
             print(stdout, stderr)
         except Exception:
             # Stop the VM and clear network interfaces in case any error prevented the start of the virtual machine.
@@ -240,33 +226,16 @@ class AlephQemuInstance(Generic[ConfigurationType], CloudInitMixin):
     async def start_guest_api(self):
         pass
 
-    #     logger.debug(f"starting guest API for {self.vm_id}")
-    #     vsock_path = f"{self.fvm.vsock_path}_53"
-    #     vm_hash = self.vm_hash
-    #     self.guest_api_process = Process(
-    #         target=run_guest_api,
-    #         args=(vsock_path, vm_hash, settings.SENTRY_DSN, settings.DOMAIN_NAME),
-    #     )
-    #     self.guest_api_process.start()
-    #     while not exists(vsock_path):
-    #         await asyncio.sleep(0.01)
-    #     await chown_to_jailman(Path(vsock_path))
-    #     logger.debug(f"started guest API for {self.vm_id}")
-
-    # async def stop_guest_api(self):
-    #     if self.guest_api_process and self.guest_api_process._popen:
-    #         self.guest_api_process.terminate()
+    async def stop_guest_api(self):
+        pass
 
     async def teardown(self):
         logger.info("Tearing down {self}")
-        pass
+        if self.enable_networking:
+            teardown_nftables_for_vm(self.vm_id)
+            if self.tap_interface:
+                await self.tap_interface.delete()
+        # await self.stop_guest_api()
 
-    #     if self.fvm:
-    #         await self.fvm.teardown()
-    #         teardown_nftables_for_vm(self.vm_id)
-    #         if self.tap_interface:
-    #             await self.tap_interface.delete()
-    #     await self.stop_guest_api()
-
-    # async def create_snapshot(self) -> CompressedDiskVolumeSnapshot:
-    #     raise NotImplementedError()
+    async def create_snapshot(self) -> CompressedDiskVolumeSnapshot:
+        raise NotImplementedError()
