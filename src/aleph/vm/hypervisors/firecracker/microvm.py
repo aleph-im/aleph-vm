aleph-im/aleph-vm

commit cc63c91a100badab5f6e503d3cc37f31295e6d9b

Fix: Remove incorrect await and fixing incorrect folders to remove before.

src/aleph/vm/hypervisors/firecracker/microvm.py
@@ -167,10 +167,10 @@ class MicroVM:
         if not self.use_jailer:
             return False
 
-        system(f"rm -fr {self.jailer_path}/net/")
-        system(f"rm -fr {self.jailer_path}/run/")
+        system(f"rm -fr {self.jailer_path}/dev/net/")
         system(f"rm -fr {self.jailer_path}/dev/kvm")
         system(f"rm -fr {self.jailer_path}/dev/urandom")
+        system(f"rm -fr {self.jailer_path}/run/")
 
         if os.path.exists(path=self.vsock_path):
             os.remove(path=self.vsock_path)

src/aleph/vm/controllers/__main__.py
@@ -77,7 +77,7 @@ async def run_instance(config: Configuration):
         init_timeout=config.vm_configuration.init_timeout,
     )
 
-    await execution.prepare_start()
+    execution.prepare_start()
     process = await execution.start(config.vm_configuration.config_file_path)
     if config.settings.PRINT_SYSTEM_LOGS:
         execution.start_printing_logs()

commit 007c832d8fff3c9b52d49981909dfa9c09cba71b

Problem: Crash when not using jailer due to inexisting folder
Solution: don't try to create the file that we won't use anyway when not using jailer

This is a regression introduced in #454

src/aleph/vm/hypervisors/firecracker/microvm.py
@@ -177,10 +177,10 @@ class MicroVM:
 
     async def save_configuration_file(self, config: FirecrackerConfig) -> Path:
         with (
-            NamedTemporaryFile(delete=False) as tmp_config_file,
-            open(f"{self.jailer_path}/tmp/config.json", "wb") as tmp_jailer_config_file,
+            NamedTemporaryFile(delete=False)
+            if not self.use_jailer
+            else open(f"{self.jailer_path}/tmp/config.json", "wb") as config_file
         ):
-            config_file = tmp_config_file if not self.use_jailer else tmp_jailer_config_file
             config_file.write(config.json(by_alias=True, exclude_none=True, indent=4).encode())
             config_file.flush()
             config_file_path = Path(config_file.name)