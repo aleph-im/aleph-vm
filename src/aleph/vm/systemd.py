f8c9780ff6024e3ce41a1135cf94f34e773996fb
Andres D. Molins <amolinsdiaz@yahoo.es>
2023-11-22 19:26:18 +0100

    Fix: Added some improvements to make it work better.

--- src/aleph/vm/systemd.py
+++ src/aleph/vm/systemd.py
@@ -22,50 +22,34 @@ class SystemDManager:
         systemd = self.bus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
         self.manager = dbus.Interface(systemd, 'org.freedesktop.systemd1.Manager')
 
-    def stop_and_disable(self, service: str) -> bool:
+    def stop_and_disable(self, service: str) -> None:
         try:
             if self.is_service_active(service):
                 self.start(service)
             if self.is_service_enabled(service):
                 self.enable(service)
-            return True
-        except:
-            return False
+        except Exception:
+            raise
 
     def enable(self, service: str) -> None:
-        try:
-            self.manager.EnableUnitFiles([service], False, True)
-            logger.debug(f"Enabled ${service} service")
-        except:
-            logger.error("Error: Failed to enable {}.".format(service))
+        self.manager.EnableUnitFiles([service], False, True)
+        logger.debug(f"Enabled {service} service")
 
     def start(self, service: str) -> None:
-        try:
-            self.manager.StartUnit(service, 'replace')
-            logger.debug(f"Started ${service} service")
-        except:
-            logger.error("Error: Failed to start {}.".format(service))
+        self.manager.StartUnit(service, 'replace')
+        logger.debug(f"Started {service} service")
 
     def stop(self, service: str) -> None:
-        try:
-            self.manager.StopUnit(service, 'replace')
-            logger.debug(f"Stopped ${service} service")
-        except:
-            logger.error("Error: Failed to stop {}.".format(service))
+        self.manager.StopUnit(service, 'replace')
+        logger.debug(f"Stopped {service} service")
 
     def restart(self, service: str) -> None:
-        try:
-            self.manager.RestartUnit(service, 'replace')
-            logger.debug(f"Restarted ${service} service")
-        except:
-            logger.error("Error: Failed to restart {}.".format(service))
+        self.manager.RestartUnit(service, 'replace')
+        logger.debug(f"Restarted {service} service")
 
     def disable(self, service: str) -> None:
-        try:
-            self.manager.DisableUnitFiles([service], False)
-            logger.debug(f"Disabled ${service} service")
-        except:
-            logger.error("Error: Failed to disable {}.".format(service))
+        self.manager.DisableUnitFiles([service], False)
+        logger.debug(f"Disabled {service} service")
 
     def is_service_enabled(self, service: str) -> bool:
         try:
@@ -80,12 +64,11 @@ class SystemDManager:
         except:
             return False
 
-    def enable_and_start(self, service: str) -> bool:
+    def enable_and_start(self, service: str) -> None:
         try:
             if not self.is_service_enabled(service):
                 self.enable(service)
             if not self.is_service_active(service):
                 self.start(service)
-            return True
-        except:
-            return False
+        except Exception:
+            raise
