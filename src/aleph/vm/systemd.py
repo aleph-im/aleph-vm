3c2b76841a6683692883fac1e6922961641a7463
Andres D. Molins <amolinsdiaz@yahoo.es>
2023-12-07 13:06:53 +0100

    Fix: Solved some PR suggestions.

--- src/aleph/vm/systemd.py
+++ src/aleph/vm/systemd.py
@@ -6,7 +6,7 @@ import logging
 import sys
 
 import dbus
-from dbus import SystemBus
+from dbus import SystemBus, DBusException
 from dbus.proxies import Interface
 
 logger = logging.getLogger(__name__)
@@ -14,60 +14,57 @@ logger = logging.getLogger(__name__)
 
 class SystemDManager:
     bus: SystemBus
-    manager: Interface
+    interface: Interface
 
     def __init__(self):
         self.bus = dbus.SystemBus()
         systemd = self.bus.get_object("org.freedesktop.systemd1", "/org/freedesktop/systemd1")
-        self.manager = dbus.Interface(systemd, "org.freedesktop.systemd1.Manager")
+        self.interface = dbus.Interface(systemd, "org.freedesktop.systemd1.Manager")
 
     def stop_and_disable(self, service: str) -> None:
-        try:
-            if self.is_service_active(service):
-                self.stop(service)
-            if self.is_service_enabled(service):
-                self.disable(service)
-        except Exception:
-            raise
+        if self.is_service_active(service):
+            self.stop(service)
+        if self.is_service_enabled(service):
+            self.disable(service)
 
     def enable(self, service: str) -> None:
-        self.manager.EnableUnitFiles([service], False, True)
+        self.interface.EnableUnitFiles([service], False, True)
         logger.debug(f"Enabled {service} service")
 
     def start(self, service: str) -> None:
-        self.manager.StartUnit(service, "replace")
+        self.interface.StartUnit(service, "replace")
         logger.debug(f"Started {service} service")
 
     def stop(self, service: str) -> None:
-        self.manager.StopUnit(service, "replace")
+        self.interface.StopUnit(service, "replace")
         logger.debug(f"Stopped {service} service")
 
     def restart(self, service: str) -> None:
-        self.manager.RestartUnit(service, "replace")
+        self.interface.RestartUnit(service, "replace")
         logger.debug(f"Restarted {service} service")
 
     def disable(self, service: str) -> None:
-        self.manager.DisableUnitFiles([service], False)
+        self.interface.DisableUnitFiles([service], False)
         logger.debug(f"Disabled {service} service")
 
     def is_service_enabled(self, service: str) -> bool:
         try:
-            return self.manager.GetUnitFileState(service) == "enabled"
-        except:
+            return self.interface.GetUnitFileState(service) == "enabled"
+        except DBusException as error:
+            logger.error(error)
             return False
 
     def is_service_active(self, service: str) -> bool:
         try:
-            self.manager.GetUnit(service)
+            self.interface.GetUnit(service)
             return True
-        except:
+        except DBusException as error:
+            logger.error(error)
             return False
 
     def enable_and_start(self, service: str) -> None:
-        try:
-            if not self.is_service_enabled(service):
-                self.enable(service)
-            if not self.is_service_active(service):
-                self.start(service)
-        except Exception:
-            raise
+        if not self.is_service_enabled(service):
+            self.enable(service)
+        if not self.is_service_active(service):
+            self.start(service)
+
