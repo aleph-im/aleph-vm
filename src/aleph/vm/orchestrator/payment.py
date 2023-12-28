e73f90b22299548a228abd2e32ec0d416e93b326
Andres D. Molins <amolinsdiaz@yahoo.es>
2023-12-28 20:04:54 +0100

    Fix: Solved some issues, type checking and get stream flow config.

--- src/aleph/vm/orchestrator/payment.py
+++ src/aleph/vm/orchestrator/payment.py
@@ -1,18 +1,20 @@
+import logging
+import math
 from decimal import Decimal
 from typing import Iterable
 
 import aiohttp
-import math
+from eth_typing import HexAddress, HexStr
+from eth_utils import from_wei, hexstr_if_str, is_address, to_hex
+from superfluid import CFA_V1, Web3FlowInfo
 
 from aleph.vm.conf import settings
-from aleph.vm.constants import GiB, MiB, Hour
+from aleph.vm.constants import GiB, Hour, MiB
 from aleph.vm.controllers.firecracker.program import AlephProgramResources
 from aleph.vm.models import VmExecution
 from aleph.vm.utils import get_path_size
 
-from eth_typing import HexAddress, HexStr
-from eth_utils import to_hex, hexstr_if_str, is_address, from_wei
-from superfluid import CFA_V1, Web3FlowInfo
+logger = logging.getLogger(__name__)
 
 
 async def get_balance(address: str) -> Decimal:
@@ -34,14 +36,15 @@ def get_stream(sender: str, receiver: str, chain) -> Decimal:
     # See https://community.aleph.im/t/pay-as-you-go-using-superfluid/98/11
 
     # TODO: Convert chain str to ID
-    superfluid_instance = CFA_V1(settings.PAYMENT_RPC_SERVER, chain)
+    chain_id = 43113
+    superfluid_instance = CFA_V1(settings.PAYMENT_RPC_SERVER, chain_id)
 
     super_token: HexAddress = to_normalized_address(settings.PAYMENT_SUPER_TOKEN)
     sender_address: HexAddress = to_normalized_address(sender)
     receiver_address: HexAddress = to_normalized_address(receiver)
 
     flow_data: Web3FlowInfo = superfluid_instance.get_flow(super_token, sender_address, receiver_address)
-    stream = from_wei(flow_data.flowRate, 'ether')
+    stream = from_wei(flow_data["flowRate"], "ether")
     return Decimal(stream)
 
 
@@ -52,18 +55,14 @@ def to_normalized_address(value: str) -> HexAddress:
     try:
         hex_address = hexstr_if_str(to_hex, value).lower()
     except AttributeError:
-        raise TypeError(
-            "Value must be any string, instead got type {}".format(type(value))
-        )
+        raise TypeError("Value must be any string, instead got type {}".format(type(value)))
     if is_address(hex_address):
         return HexAddress(HexStr(hex_address))
     else:
-        raise ValueError(
-            "Unknown format {}, attempted to normalize to {}".format(value, hex_address)
-        )
+        raise ValueError("Unknown format {}, attempted to normalize to {}".format(value, hex_address))
 
 
-async def get_required_balance(executions: Iterable[VmExecution]) -> Decimal:
+def get_required_balance(executions: Iterable[VmExecution]) -> Decimal:
     """Get the balance required for the resources of the user from the messages and the pricing aggregate."""
     balance = 0
     for execution in executions:
@@ -78,9 +77,7 @@ def compute_execution_hold_cost(execution: VmExecution) -> Decimal:
     compute_units_required = _get_nb_compute_units(execution)
     compute_unit_multiplier = _get_compute_unit_multiplier(execution)
 
-    compute_unit_price = (
-            Decimal(compute_units_required) * compute_unit_multiplier * compute_unit_cost
-    )
+    compute_unit_price = Decimal(compute_units_required) * compute_unit_multiplier * compute_unit_cost
     price = compute_unit_price + _get_additional_storage_hold_price(execution)
     return Decimal(price)
 
@@ -90,9 +87,7 @@ def _get_additional_storage_hold_price(execution: VmExecution) -> Decimal:
     free_storage_per_compute_unit = 2 * GiB if not execution.persistent else 20 * GiB
 
     total_volume_size = _get_execution_storage_size(execution)
-    additional_storage = max(
-        total_volume_size - (free_storage_per_compute_unit * nb_compute_units), 0
-    )
+    additional_storage = max(total_volume_size - (free_storage_per_compute_unit * nb_compute_units), 0)
     price = Decimal(additional_storage) / 20 / MiB
     return price
 
@@ -128,7 +123,7 @@ def _get_execution_storage_size(execution: VmExecution) -> int:
     return size
 
 
-async def get_required_flow(executions: Iterable[VmExecution]) -> Decimal:
+def get_required_flow(executions: Iterable[VmExecution]) -> Decimal:
     """Compute the flow required for the resources of the user from the messages and the pricing aggregate"""
     flow = 0
     for execution in executions:
@@ -139,27 +134,27 @@ async def get_required_flow(executions: Iterable[VmExecution]) -> Decimal:
 
 def compute_execution_flow_cost(execution: VmExecution) -> Decimal:
     compute_unit_cost_hour = 0.011 if execution.persistent else 0.11  # TODO: Get from PAYG aggregate
-    compute_unit_cost_second = compute_unit_cost_hour * Hour
+    compute_unit_cost_second = compute_unit_cost_hour / Hour
 
     compute_units_required = _get_nb_compute_units(execution)
     compute_unit_multiplier = _get_compute_unit_multiplier(execution)
 
     compute_unit_price = (
-            Decimal(compute_units_required) * compute_unit_multiplier * compute_unit_cost_second
+        Decimal(compute_units_required) * Decimal(compute_unit_multiplier) * Decimal(compute_unit_cost_second)
     )
+
     price = compute_unit_price + _get_additional_storage_flow_price(execution)
+
     return Decimal(price)
 
 
 def _get_additional_storage_flow_price(execution: VmExecution) -> Decimal:
     additional_storage_hour_price = 0.000000977  # TODO: Get from PAYG aggregate
-    additional_storage_second_price = Decimal(additional_storage_hour_price) / Hour
+    additional_storage_second_price = additional_storage_hour_price / Hour
     nb_compute_units = execution.vm.hardware_resources.vcpus
     free_storage_per_compute_unit = 2 * GiB if not execution.persistent else 20 * GiB
 
     total_volume_size = _get_execution_storage_size(execution)
-    additional_storage = max(
-        total_volume_size - (free_storage_per_compute_unit * nb_compute_units), 0
-    )
-    price = Decimal(additional_storage) / Decimal(additional_storage_second_price) / MiB
-    return price
+    additional_storage = max(total_volume_size - (free_storage_per_compute_unit * nb_compute_units), 0)
+    price = additional_storage / additional_storage_second_price / MiB
+    return Decimal(price)
