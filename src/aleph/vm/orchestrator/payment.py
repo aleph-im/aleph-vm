8a5f4f05384c341a874c0b194c60c9ad94ddede1
Andres D. Molins <amolinsdiaz@yahoo.es>
2024-01-11 12:42:57 +0100

    Fix: Solved some PR review comments.

--- src/aleph/vm/orchestrator/payment.py
+++ src/aleph/vm/orchestrator/payment.py
@@ -9,7 +9,7 @@ from eth_utils import from_wei
 from superfluid import CFA_V1, Web3FlowInfo
 
 from aleph.vm.conf import settings
-from aleph.vm.constants import GiB, Hour, MiB
+from aleph.vm.constants import GiB, HOUR, MiB
 from aleph.vm.controllers.firecracker.program import AlephProgramResources
 from aleph.vm.models import VmExecution
 from aleph.vm.utils import get_path_size, to_normalized_address
@@ -37,28 +37,28 @@ def get_stream(sender: str, receiver: str, chain) -> Decimal:
 
     # TODO: Convert chain str to ID
     chain_id = 43113
-    superfluid_instance = CFA_V1(settings.PAYMENT_RPC_SERVER, chain_id)
+    superfluid_instance = CFA_V1(settings.PAYMENT_RPC_API, chain_id)
 
     super_token: HexAddress = to_normalized_address(settings.PAYMENT_SUPER_TOKEN)
     sender_address: HexAddress = to_normalized_address(sender)
     receiver_address: HexAddress = to_normalized_address(receiver)
 
     flow_data: Web3FlowInfo = superfluid_instance.get_flow(super_token, sender_address, receiver_address)
-    # TODO:
+    # TODO: Implement and use the SDK to make the conversion
     stream = from_wei(flow_data["flowRate"], "ether")
     return Decimal(stream)
 
 
-def get_required_balance(executions: Iterable[VmExecution]) -> Decimal:
+async def compute_required_balance(executions: Iterable[VmExecution]) -> Decimal:
     """Get the balance required for the resources of the user from the messages and the pricing aggregate."""
     balance = Decimal(0)
     for execution in executions:
-        balance += compute_execution_hold_cost(execution)
+        balance += await compute_execution_hold_cost(execution)
 
     return Decimal(balance)
 
 
-def compute_execution_hold_cost(execution: VmExecution) -> Decimal:
+async def compute_execution_hold_cost(execution: VmExecution) -> Decimal:
     # TODO: Use PAYMENT_PRICING_AGGREGATE when possible
     compute_unit_cost = 200 if execution.persistent else 2000
 
@@ -66,22 +66,24 @@ def compute_execution_hold_cost(execution: VmExecution) -> Decimal:
     compute_unit_multiplier = _get_compute_unit_multiplier(execution)
 
     compute_unit_price = Decimal(compute_units_required) * Decimal(compute_unit_multiplier) * Decimal(compute_unit_cost)
-    price = compute_unit_price + _get_additional_storage_hold_price(execution)
+    additional_storage_hold_price = await _get_additional_storage_hold_price(execution)
+    price = compute_unit_price + additional_storage_hold_price
     return Decimal(price)
 
 
-def _get_additional_storage_hold_price(execution: VmExecution) -> Decimal:
+async def _get_additional_storage_hold_price(execution: VmExecution) -> Decimal:
     # TODO: Use PAYMENT_PRICING_AGGREGATE when possible
     nb_compute_units = _get_nb_compute_units(execution)
     free_storage_per_compute_unit = 2 * GiB if not execution.persistent else 20 * GiB
 
-    total_volume_size = _get_execution_storage_size(execution)
+    total_volume_size = await _get_execution_storage_size(execution)
     additional_storage = max(total_volume_size - (free_storage_per_compute_unit * nb_compute_units), 0)
     price = Decimal(additional_storage) / 20 / MiB
     return price
 
 
 def _get_nb_compute_units(execution: VmExecution) -> int:
+    """A compute unit is currently defined as: 1 vcpu, 2048 MB of memory."""
     cpu = execution.vm.hardware_resources.vcpus
     memory = math.ceil(execution.vm.hardware_resources.memory / 2048)
     nb_compute_units = cpu if cpu >= memory else memory
@@ -95,36 +97,36 @@ def _get_compute_unit_multiplier(execution: VmExecution) -> int:
     return compute_unit_multiplier
 
 
-def _get_execution_storage_size(execution: VmExecution) -> int:
+async def _get_execution_storage_size(execution: VmExecution) -> int:
     size = 0
 
     if execution.is_instance:
         size += execution.message.rootfs.size_mib * MiB
     elif execution.is_program:
         if isinstance(execution.resources, AlephProgramResources):
-            size += get_path_size(execution.resources.code_path)
+            size += await get_path_size(execution.resources.code_path)
             if execution.resources.data_path:
-                size += get_path_size(execution.resources.data_path)
+                size += await get_path_size(execution.resources.data_path)
 
     for volume in execution.resources.volumes:
-        size += get_path_size(volume.path_on_host)
+        size += await get_path_size(volume.path_on_host)
 
     return size
 
 
-def get_required_flow(executions: Iterable[VmExecution]) -> Decimal:
+async def get_required_flow(executions: Iterable[VmExecution]) -> Decimal:
     """Compute the flow required for the resources of the user from the messages and the pricing aggregate"""
     flow = Decimal(0)
     for execution in executions:
-        flow += compute_execution_flow_cost(execution)
+        flow += await compute_execution_flow_cost(execution)
 
     return Decimal(flow)
 
 
-def compute_execution_flow_cost(execution: VmExecution) -> Decimal:
+async def compute_execution_flow_cost(execution: VmExecution) -> Decimal:
     # TODO: Use PAYMENT_PRICING_AGGREGATE when possible
     compute_unit_cost_hour = 0.11 if execution.persistent else 0.011
-    compute_unit_cost_second = compute_unit_cost_hour / Hour
+    compute_unit_cost_second = compute_unit_cost_hour / HOUR
 
     compute_units_required = _get_nb_compute_units(execution)
     compute_unit_multiplier = _get_compute_unit_multiplier(execution)
@@ -133,19 +135,20 @@ def compute_execution_flow_cost(execution: VmExecution) -> Decimal:
         Decimal(compute_units_required) * Decimal(compute_unit_multiplier) * Decimal(compute_unit_cost_second)
     )
 
-    price = compute_unit_price + _get_additional_storage_flow_price(execution)
+    additional_storage_flow_price = await _get_additional_storage_flow_price(execution)
+    price = compute_unit_price + additional_storage_flow_price
 
     return Decimal(price)
 
 
-def _get_additional_storage_flow_price(execution: VmExecution) -> Decimal:
+async def _get_additional_storage_flow_price(execution: VmExecution) -> Decimal:
     # TODO: Use PAYMENT_PRICING_AGGREGATE when possible
     additional_storage_hour_price = 0.000000977
-    additional_storage_second_price = Decimal(additional_storage_hour_price) / Decimal(Hour)
+    additional_storage_second_price = Decimal(additional_storage_hour_price) / Decimal(HOUR)
     nb_compute_units = _get_nb_compute_units(execution)
     free_storage_per_compute_unit = 2 * GiB if not execution.persistent else 20 * GiB
 
-    total_volume_size = _get_execution_storage_size(execution)
+    total_volume_size = await _get_execution_storage_size(execution)
     additional_storage = max(
         Decimal(total_volume_size) - (Decimal(free_storage_per_compute_unit) * Decimal(nb_compute_units)), Decimal(0)
     )
