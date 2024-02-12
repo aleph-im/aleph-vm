58dedab94816eec795a68a7451c39d6e2d0eb0bb
Hugo Herter <git@hugoherter.com>
2024-02-12 23:26:53 +0100

    Dependencies are now in nixpkgs

--- packaging/nixos/default.nix
+++ packaging/nixos/default.nix
@@ -27,48 +27,11 @@ in
       pkgs.python311Packages.hatchling
       pkgs.python311Packages.hatch-vcs
       pkgs.python311Packages.nftables
+      pkgs.python311Packages.pyyaml
 
       pkgs.python311Packages.aiohttp
-#      unstable.python311Packages.eth-account
+      pkgs.python311Packages.eth-hash
+      pkgs.python311Packages.eth-account
+      pkgs.python311Packages.eth-typing
     ];
-
-    eth-utils = python311.pkgs.buildPythonPackage rec {
-      pname = "eth-utils";
-      version = "2.3.0";
-      src = python311.pkgs.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-CFtC9XRfRtIqGG+9hz159mp5FxwC7M14eS0d3dZy8yQ=";
-      };
-      propagatedBuildInputs = [ pkgs.python311Packages.cytoolz eth-hash ];
-    };
-
-    eth-hash = python311.pkgs.buildPythonPackage rec {
-      pname = "eth-hash";
-      version = "0.5.2";
-      src = python311.pkgs.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-G18Q7Kd2XMOF4UMO78XO1uLkY7sY0TZVEOLlOcGm/k4=";
-      };
-
-      propagatedBuildInputs = [ pkgs.python311Packages.cryptography ];
-    };
-
-    eth-typing = python311.pkgs.buildPythonPackage rec {
-      pname = "eth-typing";
-      version = "3.2.2";
-      src = python311.pkgs.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-l7oPg9p88dNmj27VSYPyEWgHbFUnYr9eBtSiCSGHfz8=";
-      };
-    };
-
-    eth-account = python311.pkgs.buildPythonPackage rec {
-      pname = "eth-account";
-      version = "0.9.0";
-      src = python311.pkgs.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-X2bst7xSVpkk369Kmt1QGxwqSQHux048BZjNJtCXF3c";
-      };
-      propagatedBuildInputs = [ eth-utils eth-hash ];
-    };
   }
