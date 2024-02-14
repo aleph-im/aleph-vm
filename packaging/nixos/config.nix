d0d150baa931a6ba8ab46089c9d77703e1b299dc
Hugo Herter <git@hugoherter.com>
2024-02-14 16:06:58 +0100

    Feature: Users can build CRN system images using nixos-generators.

--- packaging/nixos/config.nix
+++ packaging/nixos/config.nix
@@ -1,4 +1,42 @@
 { modulesPath, pkgs, config,... }:
+let
+  myPythonEnv = (
+      pkgs.python311.withPackages(ps: with ps; [
+        aiohttp
+        msgpack
+        aiodns
+        sqlalchemy
+        setproctitle
+        psutil
+        packaging
+        py-cpuinfo
+        jsonschema
+        pydantic
+
+        ( callPackage ./default.nix {} )
+
+        (
+          buildPythonPackage rec {
+            pname = "aleph-message";
+            version = "0.4.2";
+            src = fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-bfqsYb7IY7vWNfItTdcASBKBiAln7JjmZgWDdbCMPSM=";
+            };
+            doCheck = false;
+            propagatedBuildInputs = [
+              pkgs.python311Packages.pydantic
+            ];
+          }
+        )
+      ])
+    );
+
+  vmlinux = pkgs.fetchurl {
+    url = "https://ipfs.aleph.cloud/ipfs/bafybeiaj2lf6g573jiulzacvkyw4zzav7dwbo5qbeiohoduopwxs2c6vvy";
+    sha256 = "sha256-8jo6yMdVa7h3owBZ6aaxQ7mXGkg5RwoAjnjiYF77ZrY=";
+  };
+in
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   boot.initrd.availableKernelModules =
@@ -29,26 +67,27 @@
 
   services.openssh.enable = true;
 
+  services.ndppd.enable = true;
+  services.redis.enable = true;
+
 #  networking.nftables.enable = true;
 
   environment = {
     shellAliases = {
       orchestrator = "python -m aleph.vm.orchestrator";
       check-nftables = "python -m nftables";
+      check = "curl -i http://localhost:4020/status/check/fastapi";
     };
   };
 
   virtualisation = {
-    podman = {
+    docker = {
       enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       #defaultNetwork.dnsname.enable = true;
     };
-    oci-containers.backend = "podman";
+    oci-containers.backend = "docker";
     oci-containers.containers = {
       vm-connector = {
         image = "docker.io/alephim/vm-connector:alpha";
@@ -60,7 +99,6 @@
 
   environment.systemPackages = with pkgs; [
 
-
     helix
 
     git
@@ -71,46 +109,20 @@
     debootstrap
     vim
     firecracker
-    nftables
-    (
-      python311.withPackages(ps: with ps; [
-        aiohttp
-        msgpack
-        aiodns
-        sqlalchemy
-        setproctitle
-        aioredis
-        psutil
-        packaging
-        py-cpuinfo
-        nftables
-        jsonschema
-        pydantic
+    ndppd
+    cloud-utils
 
-        ( callPackage ./default.nix {} )
-
-        (
-          buildPythonPackage rec {
-            pname = "aleph-message";
-            version = "0.4.2";
-            src = fetchPypi {
-              inherit pname version;
-              sha256 = "sha256-bfqsYb7IY7vWNfItTdcASBKBiAln7JjmZgWDdbCMPSM=";
-            };
-            doCheck = false;
-            propagatedBuildInputs = [
-              pkgs.python311Packages.pydantic
-            ];
-          }
-        )
-      ])
-    )
+    myPythonEnv
   ];
 
   environment.sessionVariables = rec {
     ALEPH_VM_SETUP = "git clone https://github.com/aleph-im/aleph-vm.git";
     LD_LIBRARY_PATH = "/run/current-system/sw/lib";
     ALEPH_VM_FIRECRACKER_PATH = "/run/current-system/sw/bin/firecracker";
+    ALEPH_VM_JAILER_PATH = "/run/current-system/sw/bin/jailer";
+    ALEPH_VM_LINUX_PATH = vmlinux;
+    ALEPH_VM_SUPERVISOR_HOST = "0.0.0.0";
+    PYTHONPATH = "${myPythonEnv}/lib/";
   };
 
   system.stateVersion = "22.11";
