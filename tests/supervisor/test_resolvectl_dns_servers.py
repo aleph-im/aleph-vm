540e8f186bca194e46d80601c3d7e292006188a4
Hugo Herter <git@hugoherter.com>
2024-02-20 14:48:00 +0100

    Fix: Invalid references in unit tests failed imports
    Due to previous refactoring.

--- tests/supervisor/test_resolvectl_dns_servers.py
+++ tests/supervisor/test_resolvectl_dns_servers.py
@@ -2,17 +2,14 @@
 import os
 from unittest import mock
 
-from aleph.vm.orchestrator.conf import (
-    resolvectl_dns_servers,
-    resolvectl_dns_servers_ipv4,
-)
+from aleph.vm.conf import resolvectl_dns_servers, resolvectl_dns_servers_ipv4
 
 os.environ["ALEPH_VM_ALLOW_VM_NETWORKING"] = "False"
 
 
 def test_resolvectl():
     with mock.patch(
-        "aleph.vm.orchestrator.conf.check_output",
+        "aleph.vm.conf.check_output",
         return_value="Link 2 (eth0): 109.88.203.3 62.197.111.140\n",
     ):
         servers = {"109.88.203.3", "62.197.111.140"}
@@ -26,7 +23,7 @@ def test_resolvectl():
 
 def test_resolvectl_ipv6():
     with mock.patch(
-        "aleph.vm.orchestrator.conf.check_output",
+        "aleph.vm.conf.check_output",
         return_value="Link 2 (eth0): 109.88.203.3 62.197.111.140 2a02:2788:fff0:7::3\n        2a02:2788:fff0:5::140\n",
     ):
         ipv4_servers = {"109.88.203.3", "62.197.111.140"}
