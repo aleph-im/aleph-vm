cff288d157fef7d3f32ce6fa0d3a6b30fea17934
Hugo Herter <git@hugoherter.com>
2024-03-13 12:08:28 +0100

    Fix: Circular imports prevented pytest to run

--- src/aleph/vm/orchestrator/__init__.py
+++ src/aleph/vm/orchestrator/__init__.py
@@ -1,30 +1,3 @@
 from aleph.vm.version import __version__
 
-from . import (
-    messages,
-    metrics,
-    pubsub,
-    reactor,
-    resources,
-    run,
-    status,
-    supervisor,
-    tasks,
-    views,
-    vm,
-)
-
-__all__ = (
-    "__version__",
-    "messages",
-    "metrics",
-    "pubsub",
-    "reactor",
-    "resources",
-    "run",
-    "status",
-    "supervisor",
-    "tasks",
-    "views",
-    "vm",
-)
+__all__ = ("__version__",)
