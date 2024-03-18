67a5439a26ea74db194474b73c75643e051dcce5
Hugo Herter <git@hugoherter.com>
2024-03-18 17:13:28 +0100

    Fix: `mksquashfs` failed due to mounted filesystems
    Some environments such as NixOS mount those when doing a `chroot`.

--- runtimes/aleph-debian-12-python/create_disk_image.sh
+++ runtimes/aleph-debian-12-python/create_disk_image.sh
@@ -60,6 +60,10 @@ ln -s agetty /etc/init.d/agetty.ttyS0
 echo ttyS0 > /etc/securetty
 EOT
 
+# Some environments such as NixOS mount those when doing a `chroot`.
+umount rootfs/proc || true
+umount rootfs/sys || true
+
 cat <<EOT > ./rootfs/etc/inittab
 # /etc/inittab
 
