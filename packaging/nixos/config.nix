41b03249a495a35b871878c8bfe326569b2fde6c
Hugo Herter <git@hugoherter.com>
2024-03-18 15:59:34 +0100

    WIP: Add shortcut to build runtime

--- packaging/nixos/config.nix
+++ packaging/nixos/config.nix
@@ -65,6 +65,7 @@ in
     shellAliases = {
       run-orchestrator = "python -m aleph.vm.orchestrator";
       run-tests = "pytest --cov";
+      build-runtime = "cd ./runtimes/aleph-debian-12-python ; bash create_disk_image.sh";
       check-nftables = "python -m nftables";
       check-fastapi = "curl -i http://localhost:4020/status/check/fastapi";
       clone = "git clone https://github.com/aleph-im/aleph-vm.git";
