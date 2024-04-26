314666bbe419291c082e176a14404bc7e71a2b7c
Olivier Le Thanh Duong <olivier@lethanh.be>
2024-04-26 14:08:11 +0200

    Connect to the bus on demand to avoid having to call setup

--- src/aleph/vm/systemd.py
+++ src/aleph/vm/systemd.py
@@ -143,66 +143,73 @@ class SystemDManager:
     Used to manage the systemd services on the host on Linux.
     """
 
-    bus: Optional[MessageBus]
-    manager: Optional[SystemdProxy]
+    _bus: Optional[MessageBus] = None
+    _manager: Optional[SystemdProxy] = None
 
     def __init__(self):
         pass
 
-    async def connect(self):
-        self.bus = MessageBus(bus_type=BusType.SYSTEM)
-        await self.bus.connect()
-        path = "/org/freedesktop/systemd1"
-        bus_name = "org.freedesktop.systemd1"
-        introspect = await self.bus.introspect(bus_name, path)
-        systemd_proxy: ProxyObject = self.bus.get_proxy_object(bus_name, path, introspection=introspect)
-        interface = systemd_proxy.get_interface("org.freedesktop.systemd1.Manager")
-        # Check required method are implemented
-        assert isinstance(interface, SystemdProxy)
-        self.manager = interface
+    async def get_bus(self):
+        if self._bus is None:
+            self._bus = MessageBus(bus_type=BusType.SYSTEM)
+            await self._bus.connect()
+        return self._bus
+
+    async def get_manager(self):
+        if self._manager is None:
+            bus = await self.get_bus()
+            path = "/org/freedesktop/systemd1"
+            bus_name = "org.freedesktop.systemd1"
+            introspect = await bus.introspect(bus_name, path)
+            systemd_proxy: ProxyObject = bus.get_proxy_object(bus_name, path, introspection=introspect)
+            interface = systemd_proxy.get_interface("org.freedesktop.systemd1.Manager")
+            # Check required method are implemented
+            assert isinstance(interface, SystemdProxy)
+            self._manager = interface
+        return self._manager
 
     async def enable(self, service: str) -> None:
-        assert self.manager, "connect() not called"
-        await self.manager.call_enable_unit_files([service], False, True)
+        manager = await self.get_manager()
+        await manager.call_enable_unit_files([service], False, True)
         logger.debug(f"Enabled {service} service")
 
     async def start(self, service: str) -> None:
-        assert self.manager, "connect() not called"
-        await self.manager.call_start_unit(service, Mode.REPLACE)
+        manager = await self.get_manager()
+        await manager.call_start_unit(service, Mode.REPLACE)
         logger.debug(f"Started {service} service")
 
     async def stop(self, service: str) -> None:
-        assert self.manager, "connect() not called"
-        await self.manager.call_stop_unit(service, Mode.REPLACE)
+        manager = await self.get_manager()
+        await manager.call_stop_unit(service, Mode.REPLACE)
         logger.debug(f"Stopped {service} service")
 
     async def restart(self, service: str) -> None:
-        assert self.manager, "connect() not called"
-        await self.manager.call_restart_unit(service, Mode.REPLACE)
+        manager = await self.get_manager()
+        await manager.call_restart_unit(service, Mode.REPLACE)
         logger.debug(f"Restarted {service} service")
 
     async def disable(self, service: str) -> None:
-        assert self.manager, "connect() not called"
-        await self.manager.call_disable_unit_files([service], False)
+        manager = await self.get_manager()
+        await manager.call_disable_unit_files([service], False)
         logger.debug(f"Disabled {service} service")
 
     async def is_service_enabled(self, service: str) -> bool:
-        assert self.manager, "connect() not called"
+        manager = await self.get_manager()
         try:
-            state = await self.manager.call_get_unit_file_state(service)
+            state = await manager.call_get_unit_file_state(service)
             return state == UnitFileState.ENABLED
         except DBusError as error:
             logger.error(error)
             return False
 
     async def is_service_active(self, service: str) -> bool:
-        assert self.manager, "connect() not called"
-        assert self.bus, "connect() not called"
+        manager = await self.get_manager()
         try:
-            path = await self.manager.call_get_unit(service)
+            path = await manager.call_get_unit(service)
+            bus = await self.get_bus()
             bus_name = "org.freedesktop.systemd1"
-            introspect = await self.bus.introspect(bus_name, path)
-            systemd_service = self.bus.get_proxy_object(bus_name, path, introspection=introspect)
+            introspect = await bus.introspect(bus_name, path)
+            systemd_service = bus.get_proxy_object(bus_name, path, introspection=introspect)
             unit = systemd_service.get_interface("org.freedesktop.systemd1.Unit")
             # Check required method are implemented
             assert isinstance(unit, UnitProxy)
