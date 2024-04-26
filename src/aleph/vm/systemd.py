0758daa7c065a9a4243d77e0d42129ef798a2225
nesitor <amolinsdiaz@yahoo.es>
2024-04-26 12:58:27 +0200

    Revert "Problem: dbus call were not async"
    This reverts commit afbdd0c784b0f85b8858ecd514113dbed5df4061.

--- src/aleph/vm/systemd.py
+++ src/aleph/vm/systemd.py
@@ -2,153 +2,13 @@
 async SystemD Manager implementation.
 """
 
-import abc
-import enum
 import logging
-from typing import Literal
 
-from dbus_fast import DBusError
-from dbus_fast.aio import MessageBus, ProxyInterface, ProxyObject
+import dbus
+from dbus import DBusException, SystemBus
+from dbus.proxies import Interface
 
 logger = logging.getLogger(__name__)
-
-
-class UnitFileState(str, enum.Enum):
-    """This StrEnum class represents the different possible states of a unit file."""
-
-    ENABLED = "enabled"
-    """Indicates that a unit file is permanently enabled."""
-
-    ENABLED_RUNTIME = "enabled-runtime"
-    """Indicates the unit file is only temporarily enabled and will no longer be enabled after a reboot 
-    (that means, it is enabled via /run/ symlinks, rather than /etc/)."""
-
-    LINKED = "linked"
-    """Indicates that a unit is linked into /etc/ permanently."""
-
-    LINKED_RUNTIME = "linked-runtime"
-    """Indicates that a unit is linked into /run/ temporarily (until the next reboot)."""
-
-    MASKED = "masked"
-    """Indicates that the unit file is masked permanently."""
-
-    MASKED_RUNTIME = "masked-runtime"
-    """Indicates that it is masked in /run/ temporarily (until the next reboot)."""
-
-    STATIC = "static"
-    """Indicates that the unit is statically enabled, i.e. always enabled and doesn't need to be enabled explicitly."""
-
-    DISABLED = "disabled"
-    """Indicates that the unit file is not enabled."""
-
-    INVALID = "invalid"
-    """Indicates that it could not be determined whether the unit file is enabled."""
-
-
-UnitFileStateLiteral = Literal[
-    "enabled",
-    "enabled-runtime",
-    "linked",
-    "linked-runtime",
-    "masked",
-    "masked-runtime",
-    "static",
-    "disabled",
-    "invalid",
-]
-
-
-class Mode(str, enum.Enum):
-    REPLACE = "replace"
-    FAIL = "fail"
-    ISOLATE = "isolate"
-    IGNORE_DEPENDENCIES = "ignore-dependencies"
-    IGNORE_REQUIREMENTS = "ignore-requirements"
-
-
-class ActiveState(str, enum.Enum):
-    """
-    ActiveState contains a state value that reflects the unit's current status.
-    """
-
-    ACTIVE = "active"
-    """
-    The unit is active.
-    """
-
-    RELOADING = "reloading"
-    """
-    The unit is active and reloading its configuration.
-    """
-
-    INACTIVE = "inactive"
-    """
-    The unit is inactive, previous run was successful or hasn't yet occurred.
-    """
-
-    FAILED = "failed"
-    """
-    The unit is inactive, previous run was unsuccessful.
-    """
-
-    ACTIVATING = "activating"
-    """
-    The unit is transitioning from inactive to active state.
-    """
-
-    DEACTIVATING = "deactivating"
-    """
-    The unit is in the process of deactivation.
-    """
-
-
-ActiveStateLiteral = Literal["active", "reloading", "inactive", "failed", "activating", "deactivating"]
-
-
-class SystemdProxy(ProxyInterface, abc.ABC):
-    """ABC for typing.
-
-    for description of methodsp
-    see https://www.freedesktop.org/software/systemd/man/latest/org.freedesktop.systemd1.html#The%20Manager%20Object"""
-
-    @abc.abstractmethod
-    async def call_enable_unit_files(self, files: list[str], runtime: bool, force: bool):
-        pass
-
-    @abc.abstractmethod
-    async def call_get_unit_file_state(self, service) -> UnitFileStateLiteral:
-        pass
-
-    @abc.abstractmethod
-    async def call_start_unit(self, name, mode):
-        pass
-
-    @abc.abstractmethod
-    async def call_stop_unit(self, name, mode):
-        pass
-
-    @abc.abstractmethod
-    async def call_restart_unit(self, name, mode):
-        pass
-
-    @abc.abstractmethod
-    async def call_disable_unit_files(self, files: list[str], runtime: bool):
-        pass
-
-    @abc.abstractmethod
-    async def call_get_unit(self, name: str) -> str:
-        pass
-
-
-class UnitProxy(ProxyInterface, abc.ABC):
-    """ABC for typing.
-
-    for description of methods see
-    https://www.freedesktop.org/software/systemd/man/latest/org.freedesktop.systemd1.html#Service%20Unit%20Objects"""
-
-    @abc.abstractmethod
-    async def get_active_state(self) -> ActiveStateLiteral:
-        pass
 
 
 class SystemDManager:
@@ -157,70 +17,60 @@ class SystemDManager:
     Used to manage the systemd services on the host on Linux.
     """
 
-    bus: MessageBus
-    manager: SystemdProxy
+    bus: SystemBus
+    manager: Interface
 
     def __init__(self):
-        self.bus = MessageBus()
+        self.bus = dbus.SystemBus()
+        systemd = self.bus.get_object("org.freedesktop.systemd1", "/org/freedesktop/systemd1")
+        self.manager = dbus.Interface(systemd, "org.freedesktop.systemd1.Manager")
 
-    async def connect(self):
-        await self.bus.connect()
-        path = "/org/freedesktop/systemd1"
-        bus_name = "org.freedesktop.systemd1"
-        introspect = await self.bus.introspect(bus_name, path)
-        systemd_proxy: ProxyObject = self.bus.get_proxy_object(bus_name, path, introspection=introspect)
-        # noinspection PyTypeChecker
-        self.manager = systemd_proxy.get_interface("org.freedesktop.systemd1.Manager")  # type: ignore
+    def stop_and_disable(self, service: str) -> None:
+        if self.is_service_active(service):
+            self.stop(service)
+        if self.is_service_enabled(service):
+            self.disable(service)
 
-    async def enable(self, service: str) -> None:
-        await self.manager.call_enable_unit_files([service], False, True)
+    def enable(self, service: str) -> None:
+        self.manager.EnableUnitFiles([service], False, True)
         logger.debug(f"Enabled {service} service")
 
-    async def start(self, service: str) -> None:
-        await self.manager.call_start_unit(service, Mode.REPLACE)
+    def start(self, service: str) -> None:
+        self.manager.StartUnit(service, "replace")
         logger.debug(f"Started {service} service")
 
-    async def stop(self, service: str) -> None:
-        await self.manager.call_stop_unit(service, Mode.REPLACE)
+    def stop(self, service: str) -> None:
+        self.manager.StopUnit(service, "replace")
         logger.debug(f"Stopped {service} service")
 
-    async def restart(self, service: str) -> None:
-        await self.manager.call_restart_unit(service, Mode.REPLACE)
+    def restart(self, service: str) -> None:
+        self.manager.RestartUnit(service, "replace")
         logger.debug(f"Restarted {service} service")
 
-    async def disable(self, service: str) -> None:
-        await self.manager.call_disable_unit_files([service], False)
+    def disable(self, service: str) -> None:
+        self.manager.DisableUnitFiles([service], False)
         logger.debug(f"Disabled {service} service")
 
-    async def is_service_enabled(self, service: str) -> bool:
+    def is_service_enabled(self, service: str) -> bool:
         try:
-            state = await self.manager.call_get_unit_file_state(service)
-            return state == UnitFileState.ENABLED
-        except DBusError as error:
+            return self.manager.GetUnitFileState(service) == "enabled"
+        except DBusException as error:
             logger.error(error)
             return False
 
-    async def is_service_active(self, service: str) -> bool:
+    def is_service_active(self, service: str) -> bool:
         try:
-            path = await self.manager.call_get_unit(service)
-            bus_name = "org.freedesktop.systemd1"
-            introspect = await self.bus.introspect(bus_name, path)
-            systemd_service = self.bus.get_proxy_object(bus_name, path, introspection=introspect)
-            unit: UnitProxy = systemd_service.get_interface("org.freedesktop.systemd1.Unit")  # type: ignore
-            active_state = await unit.get_active_state()
-            return active_state == ActiveState.ACTIVE
-        except DBusError as error:
+            systemd_service = self.bus.get_object("org.freedesktop.systemd1", object_path=self.manager.GetUnit(service))
+            unit = dbus.Interface(systemd_service, "org.freedesktop.systemd1.Unit")
+            unit_properties = dbus.Interface(unit, "org.freedesktop.DBus.Properties")
+            active_state = unit_properties.Get("org.freedesktop.systemd1.Unit", "ActiveState")
+            return active_state == "active"
+        except DBusException as error:
             logger.error(error)
             return False
 
-    async def enable_and_start(self, service: str) -> None:
-        if not await self.is_service_enabled(service):
-            await self.enable(service)
-        if not await self.is_service_active(service):
-            await self.start(service)
-
-    async def stop_and_disable(self, service: str) -> None:
-        if await self.is_service_active(service):
-            await self.stop(service)
-        if await self.is_service_enabled(service):
-            await self.disable(service)
+    def enable_and_start(self, service: str) -> None:
+        if not self.is_service_enabled(service):
+            self.enable(service)
+        if not self.is_service_active(service):
+            self.start(service)
