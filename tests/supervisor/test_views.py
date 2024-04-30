03ed89ad9d50e4a0e37a24fa4633c8ae55566b79
Olivier Le Thanh Duong <olivier@lethanh.be>
2024-04-30 09:53:13 +0200

    Set up a fresh web_app for each test as required by aiohttp

--- tests/supervisor/test_views.py
+++ tests/supervisor/test_views.py
@@ -2,12 +2,13 @@ import pytest
 from aiohttp import web
 
 from aleph.vm.conf import settings
-from aleph.vm.orchestrator.supervisor import app
+from aleph.vm.orchestrator.supervisor import setup_webapp
 
 
 @pytest.mark.asyncio
 async def test_allocation_fails_on_invalid_item_hash(aiohttp_client):
     """Test that the allocation endpoint fails when an invalid item_hash is provided."""
+    app = setup_webapp()
     client = await aiohttp_client(app)
     settings.ALLOCATION_TOKEN_HASH = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"  # = "test"
     response: web.Response = await client.post(
@@ -29,6 +30,7 @@ async def test_allocation_fails_on_invalid_item_hash(aiohttp_client):
 @pytest.mark.asyncio
 async def test_system_usage(aiohttp_client):
     """Test that the allocation endpoint fails when an invalid item_hash is provided."""
+    app = setup_webapp()
     client = await aiohttp_client(app)
     settings.ALLOCATION_TOKEN_HASH = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"  # = "test"
     response: web.Response = await client.get("/about/usage/system")
