5fe46ac4e5979f397ffe4b7d875caed8f2235847
Olivier Le Thanh Duong <olivier@lethanh.be>
2024-04-30 15:03:11 +0200

    Problem: allocation endpoints was not tested
    Solution: Start by adding some simple tests
    We don't test the full allocation and deallocation here. just auth

--- tests/supervisor/test_views.py
+++ tests/supervisor/test_views.py
@@ -38,3 +38,56 @@ async def test_system_usage(aiohttp_client):
     resp = await response.json()
     assert "cpu" in resp
     assert resp["cpu"]["count"] > 0
+
+
+@pytest.mark.asyncio
+async def test_allocation_invalid_auth_token(aiohttp_client):
+    """Test that the allocation endpoint fails when an invalid auth token is provided."""
+    settings.ALLOCATION_TOKEN_HASH = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"  # = "test"
+    app = setup_webapp()
+    client = await aiohttp_client(app)
+    response = await client.post(
+        "/control/allocations",
+        json={"persistent_vms": []},
+        headers={"X-Auth-Signature": "notTest"},
+    )
+    assert response.status == 401
+    assert await response.text() == "Authentication token received is invalid"
+
+
+@pytest.mark.asyncio
+async def test_allocation_missing_auth_token(aiohttp_client):
+    """Test that the allocation endpoint fails when auth token is not provided."""
+    app = setup_webapp()
+    client = await aiohttp_client(app)
+    response: web.Response = await client.post(
+        "/control/allocations",
+        json={"persistent_vms": []},
+    )
+    assert response.status == 401
+    assert await response.text() == "Authentication token is missing"
+
+
+@pytest.mark.asyncio
+async def test_allocation_valid_token(aiohttp_client):
+    """Test that the allocation endpoint fails when an invalid auth is provided.
+
+    This is a very simple test that don't start or stop any VM so the mock is minimal"""
+
+    class FakeVmPool:
+        def get_persistent_executions(self):
+            return []
+
+    settings.ALLOCATION_TOKEN_HASH = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"  # = "test"
+    app = setup_webapp()
+    app["vm_pool"] = FakeVmPool()
+    app["pubsub"] = FakeVmPool()
+    client = await aiohttp_client(app)
+
+    response: web.Response = await client.post(
+        "/control/allocations",
+        json={"persistent_vms": []},
+        headers={"X-Auth-Signature": "test"},
+    )
+    assert response.status == 200
+    assert await response.json() == {"success": True, "successful": [], "failing": [], "errors": {}}
