9e23ee35f9c2f03e16ec35692a4ff48ec20291e6
Andres D. Molins <amolinsdiaz@yahoo.es>
2024-05-21 14:04:58 +0200

    Fix: Changed test's mocks after method refactor.

--- tests/supervisor/test_utils.py
+++ tests/supervisor/test_utils.py
@@ -4,10 +4,16 @@ from aleph.vm.utils import check_system_module
 
 
 def test_check_system_module_enabled():
+
     with mock.patch(
-        "aleph.vm.utils.subprocess.check_output",
-        return_value="Y",
+            "pathlib.Path.exists",
+            return_value=True,
     ):
         expected_value = "Y"
-        output = check_system_module("kvm_amd/parameters/sev_enp")
-        assert output == expected_value
+        with mock.patch(
+            "pathlib.Path.open",
+            mock.mock_open(read_data=expected_value),
+        ):
+
+            output = check_system_module("kvm_amd/parameters/sev_enp")
+            assert output == expected_value
