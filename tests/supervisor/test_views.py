d00800019d8618b82ae67c7e2b946974150c51d2
Olivier Le Thanh Duong <olivier@lethanh.be>
2024-06-13 13:39:54 +0200

    fix test in test_about_certificates

--- tests/supervisor/test_views.py
+++ tests/supervisor/test_views.py
@@ -168,6 +168,6 @@ async def test_about_certificates(aiohttp_client):
                 client = await aiohttp_client(app)
                 response: web.Response = await client.get("/about/certificates")
                 assert response.status == 200
-                is_file_mock.assert_has_calls([call(), call()])
+                is_file_mock.assert_has_calls([call()])
                 certificates_expected_dir = sev_client.certificates_archive
                 export_mock.assert_called_once_with(["sevctl", "export", str(certificates_expected_dir)], check=True)
