6dbbaf287da8875fd85fbee5136eeec8063f3e31
Hugo Herter <git@hugoherter.com>
2024-06-26 17:12:17 +0200

    fixup! Fix: Path to `sevctl` was not from settings

--- tests/supervisor/test_views.py
+++ tests/supervisor/test_views.py
@@ -170,4 +170,4 @@ async def test_about_certificates(aiohttp_client):
                 assert response.status == 200
                 is_file_mock.assert_has_calls([call(), call()])
                 certificates_expected_dir = sev_client.certificates_archive
-                export_mock.assert_called_once_with(["sevctl", "export", str(certificates_expected_dir)], check=True)
+                export_mock.assert_called_once_with(["/opt/sevctl", "export", str(certificates_expected_dir)], check=True)
