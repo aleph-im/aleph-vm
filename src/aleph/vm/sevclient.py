9c321463fd07932dad9b26b4539c2b2d97600c8d
Hugo Herter <git@hugoherter.com>
2024-06-27 12:46:52 +0200

    Fix: Path to `sevctl` was not from settings (#637)
    Fix: Path to `sevctl` was not from settings

--- src/aleph/vm/sevclient.py
+++ src/aleph/vm/sevclient.py
@@ -4,20 +4,27 @@ from aleph.vm.utils import run_in_subprocess
 
 
 class SevClient:
-    def __init__(self, sev_dir: Path):
+    sev_dir: Path
+    sev_ctl_executable: Path
+    certificates_dir: Path
+    certificates_archive: Path
+
+    def __init__(self, sev_dir: Path, sev_ctl_executable: Path):
         self.sev_dir = sev_dir
+        self.sev_ctl_executable = sev_ctl_executable
         self.certificates_dir = sev_dir / "platform"
         self.certificates_dir.mkdir(exist_ok=True, parents=True)
         self.certificates_archive = self.certificates_dir / "certs_export.cert"
 
-    async def sevctl_cmd(self, *args) -> bytes:
+    async def sev_ctl_cmd(self, *args) -> bytes:
+        """Run a command of the 'sevctl' tool."""
         return await run_in_subprocess(
-            ["sevctl", *args],
+            [self.sev_ctl_executable, *args],
             check=True,
         )
 
     async def get_certificates(self) -> Path:
         if not self.certificates_archive.is_file():
-            _ = await self.sevctl_cmd("export", str(self.certificates_archive))
+            _ = await self.sev_ctl_cmd("export", str(self.certificates_archive))
 
         return self.certificates_archive
