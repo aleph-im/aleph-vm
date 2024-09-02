2f7d00c126692ab14fe77211e409ff24d43c45ce
Olivier Le Thanh Duong <olivier@lethanh.be>
2024-09-02 12:16:52 +0200

    Fix CI slowness and correct  execution tests (#686)
    * Problem: Execution tests were very slow
    
    Solution: This was due to an import in the test app that is somehow
    very slow but only during testing.
    
    Haven't figured out why it is slow, but have implemented a workaround
    that delay the import so it's not hit during the tests
    
    * Fix 'real' executions test were testing the fake VM
    
    This was due to as settings contamination which made it runn the FAKE_DATA_PROGRAM instead of the real one
    
    Also correct some things that made the test not run (load_update_mesage
    instead of get_message)
    
    * Correct the Workflow name
    
    It was the same name as an other workflow which caused issue in github
    
    * Execution test were failing on Python 3.12
    
    Due to change in behaviour of unix_socket.wait_closed
    
    * Symlink don't work so make a copy instead
    
    * add vm-connector in test runner
    
    * Increase timeout for ci
    
    * Update comment src/aleph/vm/hypervisors/firecracker/microvm.py
    
    
    Co-authored-by: Hugo Herter <git@hugoherter.com>

--- tests/supervisor/test_execution.py
+++ tests/supervisor/test_execution.py
@@ -4,29 +4,35 @@ import logging
 import pytest
 from aleph_message.models import ItemHash
 
-from aleph.vm.conf import settings
+from aleph.vm.conf import Settings, settings
 from aleph.vm.controllers.firecracker import AlephFirecrackerProgram
 from aleph.vm.models import VmExecution
 from aleph.vm.orchestrator import metrics
+from aleph.vm.orchestrator.messages import load_updated_message
 from aleph.vm.storage import get_message
 
 
 @pytest.mark.asyncio
-async def test_create_execution():
+async def test_create_execution(mocker):
     """
     Create a new VM execution and check that it starts properly.
     """
+    mock_settings = Settings()
+    mocker.patch("aleph.vm.conf.settings", new=mock_settings)
+    mocker.patch("aleph.vm.storage.settings", new=mock_settings)
+    mocker.patch("aleph.vm.controllers.firecracker.executable.settings", new=mock_settings)
+    mocker.patch("aleph.vm.controllers.firecracker.program.settings", new=mock_settings)
 
-    settings.FAKE_DATA_PROGRAM = settings.BENCHMARK_FAKE_DATA_PROGRAM
-    settings.ALLOW_VM_NETWORKING = False
-    settings.USE_JAILER = False
+    mock_settings.FAKE_DATA_PROGRAM = mock_settings.BENCHMARK_FAKE_DATA_PROGRAM
+    mock_settings.ALLOW_VM_NETWORKING = False
+    mock_settings.USE_JAILER = False
 
     logging.basicConfig(level=logging.DEBUG)
-    settings.PRINT_SYSTEM_LOGS = True
+    mock_settings.PRINT_SYSTEM_LOGS = True
 
     # Ensure that the settings are correct and required files present.
-    settings.setup()
-    settings.check()
+    mock_settings.setup()
+    mock_settings.check()
 
     # The database is required for the metrics and is currently not optional.
     engine = metrics.setup_engine()
@@ -57,6 +63,7 @@ async def test_create_execution():
     await execution.stop()
 
 
+# This test depends on having a vm-connector running on port 4021
 @pytest.mark.asyncio
 async def test_create_execution_online(vm_hash: ItemHash = None):
     """
@@ -73,29 +80,34 @@ async def test_create_execution_online(vm_hash: ItemHash = None):
     engine = metrics.setup_engine()
     await metrics.create_tables(engine)
 
-    message = await get_message(ref=vm_hash)
+    message, original_message = await load_updated_message(vm_hash)
 
     execution = VmExecution(
         vm_hash=vm_hash,
         message=message.content,
-        original=message.content,
+        original=original_message.content,
         snapshot_manager=None,
         systemd_manager=None,
         persistent=False,
     )
 
-    # Downloading the resources required may take some time, limit it to 10 seconds
-    await asyncio.wait_for(execution.prepare(), timeout=30)
+    # Downloading the resources required may take some time, limit it to 120 seconds
+    # since it is a bit slow in GitHub Actions
+    await asyncio.wait_for(execution.prepare(), timeout=120)
 
     vm = execution.create(vm_id=3, tap_interface=None)
+
     # Test that the VM is created correctly. It is not started yet.
     assert isinstance(vm, AlephFirecrackerProgram)
+    vm.enable_console = True
+    vm.fvm.enable_log = True
     assert vm.vm_id == 3
 
     await execution.start()
     await execution.stop()
 
 
+# This test depends on having a vm-connector running on port 4021
 @pytest.mark.asyncio
 async def test_create_execution_legacy():
     """
