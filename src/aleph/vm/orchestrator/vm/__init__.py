32b66a54adef1075bb2e566a556a6476ae8a73dd
Olivier Le Thanh Duong <olivier@lethanh.be>
2024-10-22 15:34:41 +0200

    Display proper env conf

--- src/aleph/vm/orchestrator/vm/__init__.py
+++
@@ -1,9 +0,0 @@
-from aleph.vm.controllers.firecracker import (
-    AlephFirecrackerInstance,
-    AlephFirecrackerProgram,
-)
-
-__all__ = (
-    "AlephFirecrackerProgram",
-    "AlephFirecrackerInstance",
-)
