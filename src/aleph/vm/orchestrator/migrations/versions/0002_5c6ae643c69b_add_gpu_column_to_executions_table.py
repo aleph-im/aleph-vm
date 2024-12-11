11141af1199cc0f9dea83a7b27e2951e0d7eb62a
Andres D. Molins <amolinsdiaz@yahoo.es>
2024-12-11 20:26:28 +0100

    Fix: Updated migration to only insert the column if isn't inside.

--- src/aleph/vm/orchestrator/migrations/versions/0002_5c6ae643c69b_add_gpu_column_to_executions_table.py
+++ src/aleph/vm/orchestrator/migrations/versions/0002_5c6ae643c69b_add_gpu_column_to_executions_table.py
@@ -10,6 +10,11 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
+from sqlalchemy import create_engine
+from sqlalchemy.engine import reflection
+
+from aleph.vm.conf import make_db_url
+
 revision = "5c6ae643c69b"
 down_revision = "bbb12a12372e"
 branch_labels = None
@@ -17,7 +22,16 @@ depends_on = None
 
 
 def upgrade() -> None:
-    op.add_column("executions", sa.Column("gpus", sa.JSON(), nullable=True))
+    engine = create_engine(make_db_url())
+    inspector = reflection.Inspector.from_engine(engine)
+
+    # The table already exists on most CRNs.
+    tables = inspector.get_table_names()
+    if "executions" in tables:
+        columns = inspector.get_columns("executions")
+        column_names = [c["name"] for c in columns]
+        if "gpus" not in column_names:
+            op.add_column("executions", sa.Column("gpus", sa.JSON(), nullable=True))
 
 
 def downgrade() -> None:
