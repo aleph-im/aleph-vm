d5856db67b82a17ecef1a18aa04071317e8bab90
Antonyjin <antony.jin@epitech.eu>
2025-02-26 00:49:11 +0900

    Fix: parse_raw_as has been removed from pydantic2
    Replacing the function with TypeAdapter

--- src/aleph/vm/pool.py
+++ src/aleph/vm/pool.py
@@ -14,7 +14,7 @@ from aleph_message.models import (
     Payment,
     PaymentType,
 )
-from pydantic import parse_raw_as
+from pydantic import TypeAdapter
 
 from aleph.vm.conf import settings
 from aleph.vm.controllers.firecracker.snapshot_manager import SnapshotManager
@@ -249,7 +249,7 @@ class VmPool:
             if execution.is_running:
                 # TODO: Improve the way that we re-create running execution
                 # Load existing GPUs assigned to VMs
-                execution.gpus = parse_raw_as(List[HostGPU], saved_execution.gpus) if saved_execution.gpus else []
+                execution.gpus = TypeAdapter(List[HostGPU]).validate_python(saved_execution.gpus) if saved_execution.gpus else []
                 # Load and instantiate the rest of resources and already assigned GPUs
                 await execution.prepare()
                 if self.network:
