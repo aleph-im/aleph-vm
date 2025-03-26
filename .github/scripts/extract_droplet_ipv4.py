8589b4cd73b49a3103d10816bae8701c4f0fbb44
Olivier Le Thanh Duong <olivier@lethanh.be>
2025-03-26 12:41:14 +0100

    CI: Wait till an ipv4 on droplet before proceeding
    It might be a change in the Digital Ocean API but it return
    before the network is setup and with empty setup info
    it didn't seem to occur befor so it might be an API change from their
    part

--- .github/scripts/extract_droplet_ipv4.py
+++ .github/scripts/extract_droplet_ipv4.py
@@ -7,9 +7,22 @@ from the JSON returned by `doctl compute droplet get $name --output json
 import json
 import sys
 
+droplet_raw = sys.stdin.read()
 try:
-    droplet_info = json.load(sys.stdin)
-    print(droplet_info[0]["networks"]["v4"][0]["ip_address"])
-except Exception:
-    print(droplet_info)
+    droplet_info = json.loads(droplet_raw)
+    if not droplet_info:
+        sys.exit("No droplet info")
+    if "errors" in droplet_info:
+        sys.exit(droplet_raw)
+    if "networks" not in droplet_info[0]:
+        sys.exit(f"droplet_info[] {droplet_info[0]}")
+    elif ("v4" not in droplet_info[0]["networks"]) or not droplet_info[0]["networks"]["v4"]:
+        sys.exit("networks {}".format(droplet_info[0]["networks"]))
+    else:
+        print(droplet_info[0]["networks"]["v4"][0]["ip_address"])  # noqa: T201
+
+except Exception as e:
+    if not isinstance(e, SystemExit):
+        print(f"Failed to find ipv4: {e}", file=sys.stderr)  # noqa: T201
+        print(droplet_raw, file=sys.stderr)  # noqa: T201
     raise
