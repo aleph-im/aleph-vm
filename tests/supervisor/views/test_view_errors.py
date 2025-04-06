be148cda0c1b378894f336fbd013b42cc8fb0531
Olivier Le Thanh Duong <olivier@lethanh.be>
2025-04-06 11:16:45 +0200

    mod: setup_webapp take the VmPool as arg. VmPool remove loop arg
    mod: VmPool remove unused loop argument

--- tests/supervisor/views/test_view_errors.py
+++ tests/supervisor/views/test_view_errors.py
@@ -6,7 +6,7 @@ from aleph.vm.orchestrator.supervisor import setup_webapp
 
 @pytest.mark.asyncio
 async def test_json_404_about(aiohttp_client, mocker):
-    app = setup_webapp()
+    app = setup_webapp(pool=None)
     client: TestClient = await aiohttp_client(app)
     response = await client.get(
         "/about/non_existing_path",
@@ -18,7 +18,7 @@ async def test_json_404_about(aiohttp_client, mocker):
 
 @pytest.mark.asyncio
 async def test_json_err_allocation_notify(aiohttp_client, mocker):
-    app = setup_webapp()
+    app = setup_webapp(pool=None)
     client: TestClient = await aiohttp_client(app)
     response = await client.post("/control/allocation/notify", data="invalid_json")
     assert response.status == 400
