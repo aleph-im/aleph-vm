af3cebb27942e7fb81299e83a11d88d8909c7809
Olivier Le Thanh Duong <olivier@lethanh.be>
2025-05-05 13:38:05 +0200

    ALEPH-512 ensure we always return json for error
    Some endpoint return text for errors which made the web frontend crash
    Solution: Add a middleware which ensure a json error is always returned
    
    Add some test for it

---
+++ tests/supervisor/views/test_view_errors.py
@@ -0,0 +1,26 @@
+import pytest
+from aiohttp.test_utils import TestClient
+
+from aleph.vm.orchestrator.supervisor import setup_webapp
+
+
+@pytest.mark.asyncio
+async def test_json_404_about(aiohttp_client, mocker):
+    app = setup_webapp()
+    client: TestClient = await aiohttp_client(app)
+    response = await client.get(
+        "/about/non_existing_path",
+    )
+    assert response.status == 404
+    assert response.content_type == "application/json"
+    assert await response.json() == {"error": "404: Not Found"}
+
+
+@pytest.mark.asyncio
+async def test_json_err_allocation_notify(aiohttp_client, mocker):
+    app = setup_webapp()
+    client: TestClient = await aiohttp_client(app)
+    response = await client.post("/control/allocation/notify", data="invalid_json")
+    assert response.status == 400
+    assert response.content_type == "application/json"
+    assert await response.json() == {"error": "Body is not valid JSON"}
