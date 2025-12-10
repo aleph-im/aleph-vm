4c0c739e2ddfdb67ab7ee211e291e60a37b898b7
Andres D. Molins <amolinsdiaz@yahoo.es>
2025-12-10 12:44:18 +0100

    Fix: Solve failing test replacing by 2 proper ones.

--- tests/supervisor/test_resources.py
+++ tests/supervisor/test_resources.py
@@ -1,6 +1,40 @@
 from unittest import mock
 
-from aleph.vm.resources import get_gpu_devices
+import pytest
+from aleph_message.models import InstanceContent, InstanceMessage
+
+from aleph.vm.resources import (
+    InsufficientResourcesError,
+    check_sufficient_resources,
+    get_gpu_devices,
+)
+
+
+@pytest.fixture()
+def mock_instance_content():
+    fake = {
+        "address": "0x9319Ad3B7A8E0eE24f2E639c40D8eD124C5520Ba",
+        "time": 1713874241.800818,
+        "allow_amend": False,
+        "metadata": None,
+        "authorized_keys": None,
+        "variables": None,
+        "environment": {"reproducible": False, "internet": True, "aleph_api": True, "shared_cache": False},
+        "resources": {"vcpus": 4, "memory": 2048, "seconds": 30, "published_ports": None},
+        "payment": {"type": "credit", "chain": "BASE"},
+        "requirements": None,
+        "replaces": None,
+        "rootfs": {
+            "parent": {"ref": "63f07193e6ee9d207b7d1fcf8286f9aee34e6f12f101d2ec77c1229f92964696"},
+            "ref": "63f07193e6ee9d207b7d1fcf8286f9aee34e6f12f101d2ec77c1229f92964696",
+            "use_latest": True,
+            "comment": "",
+            "persistence": "host",
+            "size_mib": 10240,
+        },
+    }
+
+    return fake
 
 
 def mock_is_kernel_enabled_gpu(pci_host: str) -> bool:
@@ -34,3 +68,40 @@ def test_get_gpu_devices():
             assert expected_gpu_devices[0].device_class == "0300"
             assert expected_gpu_devices[0].pci_host == "01:00.0"
             assert expected_gpu_devices[0].device_id == "10de:27b0"
+
+
+def test_check_sufficient_resources(mocker, mock_instance_content):
+    required = {"vcpus": 4, "memory_mb": 2048, "disk_mb": 10240}
+
+    mocker.patch("aleph.vm.orchestrator.resources.psutil.cpu_count", return_value=required["vcpus"])
+    mocker.patch(
+        "aleph.vm.orchestrator.resources.psutil.virtual_memory",
+        return_value=mocker.MagicMock(available=(required["memory_mb"] * 1000 * 1024)),
+    )
+
+    content = InstanceContent.model_validate(mock_instance_content)
+
+    check_sufficient_resources((required["disk_mb"] * 1000 * 1024), content)
+
+
+def test_check_sufficient_resources_not_enough(mocker, mock_instance_content):
+    required = {"vcpus": 4, "memory_mb": 2048, "disk_mb": 10240}
+    available = {"vcpus": 2, "memory_mb": 1024, "disk_mb": 5120}
+    error = InsufficientResourcesError(
+        "Insufficient resources to create VM. vCPUs: required 4, available 2; "
+        "Memory: required 2048 MB, available 1024.00 MB; "
+        "Disk: required 10240 MB, available 5120.00 MB",
+        required=required,
+        available=available,
+    )
+
+    mocker.patch("aleph.vm.orchestrator.resources.psutil.cpu_count", return_value=available["vcpus"])
+    mocker.patch(
+        "aleph.vm.orchestrator.resources.psutil.virtual_memory",
+        return_value=mocker.MagicMock(available=(available["memory_mb"] * 1000 * 1024)),
+    )
+
+    content = InstanceContent.model_validate(mock_instance_content)
+
+    with pytest.raises(InsufficientResourcesError, match=str(error)):
+        check_sufficient_resources((available["disk_mb"] * 1000 * 1024), content)
