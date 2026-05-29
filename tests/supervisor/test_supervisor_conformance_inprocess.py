45e01e2f9741a4f1a0e76ca3e8c4210204943db4
Olivier Desenfans <desenfans.olivier@gmail.com>
2026-05-29 02:26:20 +0200

    test(supervisor): reusable conformance suite bound to InProcessSupervisor

---
+++ tests/supervisor/test_supervisor_conformance_inprocess.py
@@ -0,0 +1,16 @@
+import pytest
+
+from conformance import SupervisorContractTests
+
+from aleph.vm.supervisor.inprocess import InProcessSupervisor
+
+
+class FakePool:
+    def __init__(self):
+        self.executions = {}
+
+
+class TestInProcessSupervisorConformance(SupervisorContractTests):
+    @pytest.fixture
+    def supervisor(self):
+        return InProcessSupervisor(pool=FakePool())
